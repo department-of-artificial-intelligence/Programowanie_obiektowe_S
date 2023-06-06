9427f8db6bc79833930c87613e42e9a8e296ab65
Rafal Grycuk <rafal@smth.smth>
2023-06-06 22:13:59 +0200

    Images

--- Kolokwium/Kolokwium.DAL/EF/ApplicationDbContext.cs
+++ Kolokwium/Kolokwium.DAL/EF/ApplicationDbContext.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 namespace Kolokwium.DAL.EF;
 public class ApplicationDbContext : DbContext
 {
-    public virtual DbSet<Person> Persons { get; set; } = null!;
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {

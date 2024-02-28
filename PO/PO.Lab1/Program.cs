e0a5fdc82237d7f40fd94351129626d717ebca3f
Stanislav_Ivantsov <smth@smth.smth>
2024-02-28 17:42:52 +0100

    Lab1 End lesson

--- PO/PO.Lab1/Program.cs
+++ PO/PO.Lab1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

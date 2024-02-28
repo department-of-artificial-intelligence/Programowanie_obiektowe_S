5a14ff9cb8d1ed3f6eaf6245d3a41454ccb5a9b2
Danylo Latypov <smth@smth.smth>
2024-02-28 17:42:25 +0100

    public class Car {     string _brand;     string _model;     private static int _doorCount;     double _engineVolume;     double _avgConsump; //średniego spalania na 100 km
        public Car()
        {
            _brand = "none";
            _model = "model";
            _doorCount++;
            _engineVolume = 0;
            _avgConsump = 0;
        }
    
        public Car(string brand, string model, double engineVolume, double avgConsump)
        {
            _brand = brand;
            _model = model;
            _engineVolume = engineVolume;
            _avgConsump = avgConsump;
        }
    
        public decimal CalculateConsump(decimal consumpValuePerSquareMeter)
        {
            return consumpValuePerSquareMeter * (decimal)_avgConsump;
        }
    
        public decimal CalculateCost(decimal costValue)
        {
            return costValueValue
        }
    }

--- PO.Lab1/Program.cs
+++ PO.Lab1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Car
+{
+    string _brand;
+    string _model;
+    private static int _doorCount;
+    double _engineVolume;
+    double _avgConsump; //średniego spalania na 100 km
+
+    public Car()
+    {
+        _brand = "none";
+        _model = "model";
+        _doorCount++;
+        _engineVolume = 0;
+        _avgConsump = 0;
+    }
+
+    public Car(string brand, string model, double engineVolume, double avgConsump)
+    {
+        _brand = brand;
+        _model = model;
+        _engineVolume = engineVolume;
+        _avgConsump = avgConsump;
+    }
+
+    public decimal CalculateConsump(decimal consumpValuePerSquareMeter)
+    {
+        return consumpValuePerSquareMeter * (decimal)_avgConsump;
+    }
+
+    public decimal CalculateCost(decimal costValue)
+    {
+        return costValueValue 
+    }
+}
+

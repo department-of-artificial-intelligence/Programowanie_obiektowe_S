dc33fa879384735f8c7fd8da6b80ab77bf91e4d6
Oskar_Jagielski <msth@msth.pl>
2024-03-13 15:50:08 +0100

    koniec Lab02

---
+++ PO/Lab02/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    internal class Person
+    {
+        protected string _firstName;
+        protected string _lastName;
+        protected DateTime _dateOfBirth;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+
+        public Person()
+        {
+            _firstName = "nieznane";
+            _lastName = "nieznane";
+            _dateOfBirth = DateTime.MinValue;
+        }
+
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _dateOfBirth = dateOfBirth;
+        }
+
+        public override string ToString()
+        {
+            return $"Person | First name: {_firstName}, Last name: {_lastName}, Date of birth: {_dateOfBirth}";
+        }
+
+        public virtual void Details()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
---
+++ PO/Lab02/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    internal class Player : Person
+    {
+        private string _position;
+        private string _club;
+        private int _scoredGoals;
+
+        public string Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public string Club
+        {
+            get { return _club; }
+            set { _club = value; }
+        }
+
+        public int ScoredGoals
+        {
+            get { return _scoredGoals; }
+            set { _scoredGoals = value; }
+        }
+
+        public Player()
+        {
+            _position = "nieznana";
+            _club = "nieznany";
+            _scoredGoals = 0;
+        }
+
+        public Player(string firstName, string lastName, DateTime dateOfBirth, string position, string club, int scoredGoals)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _dateOfBirth = dateOfBirth;
+            _position = position;
+            _club = club;
+            _scoredGoals = scoredGoals;
+        }
+
+        public override string ToString()
+        {
+            return $"Person | Player | First name: {_firstName}, Last name: {_lastName}, Date of birth: {_dateOfBirth}, Position: {_position}, Club: {_club}, Scored goals: {_scoredGoals}";
+        }
+
+        public void ScoreGoal()
+        {
+            _scoredGoals++;
+        }
+    }
+}

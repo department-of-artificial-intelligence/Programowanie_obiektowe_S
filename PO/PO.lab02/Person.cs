1093c229ae978ab461fe2e8c8098439ed1e711cf
Jan_Kowalski <Student@lab.lab>
2024-03-13 16:44:25 +0100

    lab02 zadanie 1

---
+++ PO/PO.lab02/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO.lab02
+{
+    internal class Person
+    {
+
+        protected string _firstName = null!;
+        protected string _lastName = null!;
+
+        protected DateTime _dateOfBirth;
+
+        public string FirstName { 
+            get { return _firstName; } 
+            set { _firstName = value; } 
+        }
+
+        public string LastName { 
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+
+        public Person()
+        {
+            FirstName = "";
+            LastName = "";
+            DateOfBirth = DateTime.MinValue;
+        }
+
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public override string ToString()
+        {
+            return $"Person | FirstName: {FirstName}, LastName: {LastName}, Date of birth: {DateOfBirth}";
+        }
+
+        public virtual void Details()
+        {
+            base.ToString();
+        }
+
+    }
+}
--- PO/PO.lab1/Garage.cs
+++ PO/PO.lab1/Garage.cs
@@ -8,22 +8,23 @@ namespace PO.lab1
 {
     internal class Garage
     {
-        private Car[] _cars;
-        private string _address = null!;
-        private int? _carCount;
-        private int? _capacity;
+        //private Car[] _cars;
+        //private string _address = null!;
+        //private int? _carCount;
+        //private int? _capacity;
 
         
-        public Car[]? Cars { get => _cars; set => _cars = value; }
-        public string Address { get => _address; set => _address = value; }
-        public int? CarCount { get => _carCount; set => _carCount = value; }
-        public int? Capacity 
-        { 
-            get { return _capacity }
-            set { 
-                _capacity = value;
-                _cars = new Car[value];
-            }
-        }
+        //public Car[]? Cars { get => _cars; set => _cars = value; }
+        //public string Address { get => _address; set => _address = value; }
+        //public int? CarCount { get => _carCount; set => _carCount = value; }
+        //public int? Capacity
+        //{
+        //    get { return _capacity }
+        //    set
+        //    {
+        //        _capacity = value;
+        //        _cars = new Car[value];
+        //    }
+        //}
     }
 }

dde4319b01920908792db79fc296b2f6a780b291
Oskar_Jagielski <smth@smth.smth>
2024-03-17 19:40:33 +0100

    koniec

---
+++ PO/PO.Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using PO.Lab01;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Car();
+        car1.Details();
+        car1.Brand = "Fiat";
+        car1.Model = "126p";
+        car1.DoorCount = 2;
+        car1.EngineVolume = 650;
+        car1.AvgConsump = 6.0;
+        car1.Details();
+        Car car2 = new Car("Syrena", "105", 2, 0.8f, 7.6d);
+        car2.Details();
+        Console.WriteLine(car1);
+        double routeConsumption = car2.CalculateConsump(500);
+        Console.WriteLine($"Route consumption: {routeConsumption} l");
+        double routeCost = car2.CalculateCost(500, 5);
+        Console.WriteLine($"Route cost: {routeCost}");
+        Car.DisplayCarCount();
+        Console.WriteLine("\r\n=========================================\r\n");
+    }
+}

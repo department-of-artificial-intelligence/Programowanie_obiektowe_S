d5c3930766a18e442e950981568c10c716855d18
Oskar_Jagielski <smth@smth.smth>
2024-03-17 19:26:18 +0100

    koniec

--- PO/Lab02/Program.cs
+++ PO/Lab02/Program.cs
@@ -1,4 +1,4 @@
-﻿using Lab02;
+﻿using PO.Lab02;
 
 public class Program
 {
@@ -7,14 +7,11 @@ public class Program
         Person person1 = new Person("Adam", "Miś", new DateTime(1990, 3, 20, 12, 30, 10));
         Person person2 = new Student("Michał", "Kot", new DateTime(1990, 4, 13), 3, 5, 12345);
         Person person3 = new Player("Robert", "Lewandowski", new DateTime(1988, 10, 3), "Striker", "Bayern", 41);
-
         person1.Details();
         person2.Details();
         person3.Details();
-
         Student student = new Student("Krzysztof", "Jeż", new DateTime(1990, 12, 29), 2, 5, 54321);
         student.Details();
-
         ((Player) person3).ScoreGoal();
         person3.Details();
     }
---
+++ PO/PO.Lab1/Car.cs
@@ -0,0 +1,90 @@
+﻿namespace PO.Lab01
+{
+    internal class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private float _engineVolume;
+        private double _avgConsump;
+        private static int _carCount = 0;
+
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set { _doorCount = value; }
+        }
+
+        public float EngineVolume
+        {
+            get { return _engineVolume; }
+            set { _engineVolume = value; }
+        }
+
+        public double AvgConsump
+        {
+            get { return _avgConsump; }
+            set { _avgConsump = value; }
+        }
+
+        public Car()
+        {
+            _brand = "nieznana";
+            _model = "nieznany";
+            _doorCount = 0;
+            _engineVolume = 0f;
+            _avgConsump = 0.0;
+            _carCount++;
+        }
+
+        public Car(string brand, string model, int doorCount, float engingeVolume, double avgConsump)
+        {
+            _brand = brand;
+            _model = model;
+            _doorCount = doorCount;
+            _engineVolume = engingeVolume;
+            _avgConsump = avgConsump;
+            _carCount++;
+        }
+
+        public double CalculateConsump(double roadLength)
+        {
+            double consump = (AvgConsump * roadLength) / 100.0;
+            return consump;
+        }
+
+        public double CalculateCost(double roadLength, double petrolCost)
+        {
+            double cost = CalculateConsump(roadLength) * petrolCost;
+            return cost;
+        }
+
+        public override string ToString()
+        {
+            return $"Car | Brand: {_brand}, Model: {_model}, NumOfDoors: {_doorCount}, EngineVol: {_engineVolume}, AvgConsump: {_avgConsump}";
+        }
+
+        public void Details()
+        {
+            Console.WriteLine(this);
+
+        }
+
+        public static void DisplayCarCount()
+        {
+            Console.WriteLine(_carCount);
+        }
+    }
+}

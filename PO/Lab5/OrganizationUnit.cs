3e4acd63160d253031274806e582ce5630b32c81
Dawid_Klyta <jacekplacek@racuchy.eu>
2024-03-27 15:53:20 +0100

    lab5

---
+++ PO/Lab5/OrganizationUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Lab5
+{
+    public class OrganizationUnit
+    {
+        public string Name {get; set;}
+        public string Address { get; set; }
+        public List<Lecturer> Lecturers { get; set; }
+        public OrganizationUnit(string name, string address, List<Lecturer> lecturers)
+        {
+            Name = name;
+            Address = address;
+            Lecturers = lecturers;
+        }
+
+        public override string ToString()
+        {
+            string result= "OrganizationUnit | Name: " + Name + ", Address: " + Address + "\nLecturers:";
+            Lecturers.
+            foreach( Lecturer lec in Lecturers)
+            {
+                result+= "\n" + lec.ToString();
+            }
+            return result;
+        }
+    }
+}
---
+++ PO/Lab5/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Lab5;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}

c8e4db31fb2127e96e9ea9c0337713be131af91e
Maksym_Nievierov <norbert123123123@gmail.com>
2024-03-27 17:51:30 +0100

    lab5_1

---
+++ PO/Lab5/Lab5.BLL/Mammal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab5.BLL
+{
+    public class Mammal
+    {
+        private string _species = String.Empty;
+        private string _typeOfFood = String.Empty;
+        private string _origin = String.Empty;
+        private string _naturalArea = String.Empty;
+    }
+}

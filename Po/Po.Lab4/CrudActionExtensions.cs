0b22e47bcdeb5b0b6f8811aa721c11b0b8cdddd1
Marcin-gif <63023436+Marcin-gif@users.noreply.github.com>
2024-03-28 01:02:24 +0100

    s

--- Po/Po.Lab4/CrudActionExtensions.cs
+++ Po/Po.Lab4/CrudActionExtensions.cs
@@ -9,13 +9,22 @@ namespace Po.Lab4
 {
     internal static class CrudActionExtensions
     {
-        public static IList<TObjectType> Set<TObjectType>(this IContainer containerObject)
-        { 
+        public static IList<TObjectType>? Set<TObjectType>(this IContainer containerObject)
+        {
             var containerObjectType = containerObject.GetType();
             var propertyInfo = containerObjectType.GetProperties().FirstOrDefault(
                 p => p.PropertyType == typeof(IList<TObjectType>));
-            var value = propertyInfo?.GetValue(containerObject);
-            return value as IList<TObjectType>;
+
+            if (propertyInfo != null)
+            {
+                var value = propertyInfo.GetValue(containerObject);
+                return value as IList<TObjectType>;
+            }
+            else
+            {
+                // Możesz tu również rzucić wyjątek lub zwrócić pustą listę, w zależności od wymagań aplikacji.
+                return new List<TObjectType>();
+            }
         }
         public static void ForEach<TObjectType>(this IList<TObjectType> list, Action<TObjectType> action)
         {
@@ -41,6 +50,63 @@ namespace Po.Lab4
                 throw new InvalidOperationException("Container does not contain a list of specified type");
             }
 
+        }
+        public static IList<TObjectType> GetList<TObjectType>(this IContainer container, Func<TObjectType, bool> searchPredicate)
+        {
+            var containerObjectType = container.GetType();
+            var propertyInfo = containerObjectType.GetProperties().FirstOrDefault(
+                p=>p.PropertyType == typeof(IList<TObjectType>));
+            var value = propertyInfo?.GetValue(container);
+            var list = value as IList<TObjectType>;
+            if (list != null)
+            { 
+                return list.ToList(); 
+            } 
+            else {
+                throw new InvalidOperationException("Container does not contain a list of the specified type");
+            }
+        }
+        public static IContainer Add<TObjectType>(this IContainer container, TObjectType obj)
+        { 
+            container.Set<TObjectType>()?.Add(obj);
+            return container;
+        }
+        public static bool Remove<TObjectType>(this IContainer container, Func<TObjectType, bool> searchFn)
+        { 
+            var containerObjectType = container.GetType();
+            var propertyInfo = containerObjectType.GetProperties().FirstOrDefault(
+                p=>p.PropertyType == typeof(IList<TObjectType>));
+            var value = propertyInfo?.GetValue(container);
+            var list = value as IList<TObjectType>;
+            if (list != null)
+            {
+                var itemsToRemove = list.Where(searchFn).ToList();
+                foreach (var item in itemsToRemove)
+                {
+                    list.Remove(item);
+                }
+                return true;
+            }
+            else { 
+                return false;
+            }
+        }
+        public static IContainer AddRange<TObjectType>(this IContainer container, IList<TObjectType> listOfElement)
+        {
+            var containerObjectType = container.GetType();
+            var propertyInfo = containerObjectType.GetProperties().FirstOrDefault(
+                p=>p.PropertyType == typeof(IList<TObjectType>));
+            var value = propertyInfo?.GetValue(container);
+            var list = new List<TObjectType>();
+
+            if (list != null)
+            {
+                foreach (var element in listOfElement)
+                { 
+                    list.Add(element); 
+                }
+            }
+            return container;
         }
     }
 }
---
+++ Po/Po.Lab4/DisplayActionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Po.Lab4
+{
+    internal class DisplayActionExtensions
+    {
+        public void Print<TObjectType>(TObjectType obj)
+        {
+            Console.WriteLine(obj);
+        }
+        public void Print<TObjectType>(IList<TObjectType> list)
+        {
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}

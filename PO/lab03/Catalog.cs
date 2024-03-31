82ebef24b7fca5bfe26d08d49a6a3023fd5a4444
Mikołaj Taborek <smth@smth.smth>
2024-03-31 14:43:20 +0200

    31.03.2024 14:45

---
+++ PO/lab03/Catalog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO.lab03
+{
+    public class Catalog
+    {
+        public IList<Item>?Items
+        { get; set; }
+        public string ThematicDepartment
+        { get; set; }
+        public Catalog(IList<Item>items) 
+        {
+            Items = items;
+        }
+        public Catalog(string thematicDepartment, IList<Item> items) 
+        {
+            ThematicDepartment = thematicDepartment;
+            Items = items;
+        }
+
+    }
+}

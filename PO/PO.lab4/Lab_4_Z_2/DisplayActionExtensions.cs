95e71d9b38c34e318a1261db1eb2c6d4f0928c2c
Bohdan Ferents <bogda@FANTOM>
2024-04-02 23:02:24 +0200

    Lab_4_Z_2

---
+++ PO/PO.lab4/Lab_4_Z_2/DisplayActionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Przygotowanie
+{
+    internal static class DisplayActionExtensions
+    {
+
+        public static void Print<TObjectType>(this TObjectType obj) {
+
+            Console.WriteLine(obj?.ToString());
+
+        }
+
+        public static void Print<TObjectType>(this IList<TObjectType> list) { 
+        
+            foreach (var item in list) {
+
+                if (list != null) {
+
+                    Console.WriteLine(item?.ToString());
+                }
+
+            }
+        
+        }
+
+
+    }
+}
---
+++ PO/PO.lab4/Lab_4_Z_2/OrganizationUnit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Przygotowanie
+{
+    internal class OrganizationUnit : IContainer, IDisplayable
+    {
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public IList<Lecturer> Lecturers { get; set; }
+
+        public OrganizationUnit(string name, string address, IList<Lecturer> lecturers) { 
+        
+            Name = name;
+            Address = address;
+            Lecturers = lecturers;
+
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Name: {Name}");
+            sb.AppendLine($"Address: {Address}");
+            sb.AppendLine($"Lecturers:");
+
+            foreach (var lecturer in Lecturers) {
+
+
+                sb.AppendLine(lecturer.ToString());
+            
+            
+            }
+
+            return sb.ToString();
+
+        }
+
+    }
+}

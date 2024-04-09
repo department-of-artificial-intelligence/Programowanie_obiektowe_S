33bfd56993337a198e67364a351bdda209f5c709
Maksym Nievierov <m.aksneverov2005@gmail.com>
2024-04-09 21:00:24 +0200

    lab4

---
+++ PO/PO.Lab4/lab4.app/Lecturer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4.app
+{
+    internal class Lecturer : Person
+    {
+        public string AcademicTitle { get; set; }
+        public string Position { get; set; }
+        public Lecturer(string firstName, string lastName, DateTime dateOfBirth, string academicTitle, string position) : base(firstName, lastName, dateOfBirth)
+        {
+            AcademicTitle = academicTitle;
+            Position = position;
+        }
+        public override string ToString()
+        {
+            return $"Lecturer | " + base.ToString() +$"Academic Title: {AcademicTitle} Position: {Position}";
+        }
+    }
+}
---
+++ PO/PO.Lab4/lab4.app/OrganizationUnit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4.app
+{
+    internal class OrganizationUnit
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public IList<Lecturer> Lecturers { get; set; }
+
+        public OrganizationUnit(string name, string address, IList<Lecturer> lecturers)
+        {
+            Name = name;
+            Address = address;
+            Lecturers = lecturers;
+        }
+        public override string ToString()
+        {
+            string s = new string($"Organization Unit | Name: {Name} Address: {Address} Lecturers: \n");
+            foreach (var item in Lecturers)
+            {
+                s += item.ToString();
+            }
+            return s;
+        }
+    }
+}

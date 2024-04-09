09996bc18ef93468641cdc5447b6c6e8a16cef4b
mateusz_kapitula <matkapitulten@gmail.com>
2024-04-10 01:23:04 +0200

    Lab 4 i Lab5 ready

---
+++ Lab5/Lab5.Bll/Department.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lab5.Bll
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public Person Dean { get; set; }
+        public IList<OrganizationUnit> ogranizationUnits { get; set; }
+        public IList<Subject> Subjects { get; set; }
+        public IList<Student> Students { get; set; }
+        public Department(string name, Person dean, IList<Subject> subjects, IList<Student> students)
+        {
+            Name = name;
+            Dean = dean;
+            ogranizationUnits = new List<OrganizationUnit>();
+            Subjects = subjects;
+            Students = students;
+        }
+        public override string ToString()
+        {
+            string sub = string.Empty;
+            string stud = string.Empty;
+            sub += string.Join("\n", Subjects);
+            stud += string.Join("\n", Students);
+
+            return $"{Name} \nDziekan:\n{Dean} \nPrzedmioty:\n{sub} \nStudenci:\n{stud}";
+        }
+    }
+}
---
+++ Lab5/Lab5.Bll/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.Bll
+{
+    public class Student : Person
+    {
+        private static int _id;
+
+        public IList<FinalGrade> Gradess { get; set; }
+        public int Semester { get; set; }
+        public int Group { get; set; }
+        public int IndexId { get; set; }
+        public string Specjalization { get; set; }
+        public double AverageGrades { get; set; }
+        public Student(string firstName, string lastName, DateTime dateOfBirth, string specjalization, int group, int semester = 1) : base(firstName, lastName, dateOfBirth)
+        {
+            Specjalization = specjalization;
+            Group = group;
+            Semester = semester;
+            Gradess = new List<FinalGrade>();
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Specjalization: {Specjalization} Grupa: {Group} Semester: {Semester}";
+        }
+
+    }
+}

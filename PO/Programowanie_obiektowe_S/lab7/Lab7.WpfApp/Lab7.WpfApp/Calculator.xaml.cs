ec0fc4abaa375839457f45ddc21f19a09e3ebab9
Artem_Grun <qwer@qwer.com>
2024-04-10 17:52:50 +0200

    lab7

---
+++ PO/Programowanie_obiektowe_S/lab7/Lab7.WpfApp/Lab7.WpfApp/Calculator.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab7.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        double rezult = 0; 
+        string str = string.Empty;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button button){
+                TextBox.Text += button.Content;
+            }
+            
+                
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                TextBox.Text += button.Content;
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                TextBox.Text += button.Content;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                TextBox.Text += button.Content;
+            }
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                TextBox.Text += button.Content;
+                
+            }
+            
+        }
+
+    }
+}

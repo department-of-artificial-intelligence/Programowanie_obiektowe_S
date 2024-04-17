c674992dcbc5264f944ed919d7fb6a5ec647d24f
Mateusz_Kulik <smth@smth.smth>
2024-04-17 16:52:28 +0200

    Po.Lab7

---
+++ PO/Po.Lab7/lab7/lab7/Calculator.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace lab7.BLL
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void b9_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonRownaSie_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button bRownaSie)
+            {
+                Wyswietlanie.Text = (string)bRownaSie.Content;
+            }
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button b1)
+            {
+                Wyswietlanie.Text += (string)b1.Content;
+            }
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

90bb4d2f7be43688bf5017ecb7f43959c5315d80
Mikolaj Szczot <smth@smth.smth>
2024-04-23 23:57:50 +0200

    lab 7 kolejne

--- LabSiedem/LabSiedem.WpfApp/Calculator.xaml.cs
+++ LabSiedem/LabSiedem.WpfApp/Calculator.xaml.cs
@@ -16,165 +16,45 @@ using System.Windows.Shapes;
 
 namespace LabSiedem.WpfApp
 {
-    /// <summary>
-    /// Logika interakcji dla klasy Calculator.xaml
-    /// </summary>
     public partial class Calculator : Window
     {
         float result, num1;
         int operation;
+
         public Calculator()
         {
+            Button0.Click += NumberButtonClick;
+            Button1.Click += NumberButtonClick;
+            Button2.Click += NumberButtonClick;
+            Button3.Click += NumberButtonClick;
+            Button4.Click += NumberButtonClick;
+            Button5.Click += NumberButtonClick;
+            Button6.Click += NumberButtonClick;
+            Button7.Click += NumberButtonClick;
+            Button8.Click += NumberButtonClick;
+            Button9.Click += NumberButtonClick;
+            ButtonDot.Click += NumberButtonClick;
             InitializeComponent();
         }
 
-        private void Button0_Click(object sender, RoutedEventArgs e)
+        private void NumberButtonClick(object sender, EventArgs e)
         {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "0";
-            }
-            else
-            {
-                outbox.Text += "0";
-            }
+            Button button = (Button)sender;
+            outbox.Text += button.Content;
         }
-
-        private void Button1_Click(object sender, RoutedEventArgs e)
-        {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "1";
-            }
-            else
-            {
-                outbox.Text += "1";
-            }
-        }
-
-        private void Button2_Click(object sender, RoutedEventArgs e)
-        {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "2";
-            }
-            else
-            {
-                outbox.Text += "2";
-            }
-        }
-
-        private void Button3_Click(object sender, RoutedEventArgs e)
-        {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "3";
-            }
-            else
-            {
-                outbox.Text += "3";
-            }
-        }
-
-        private void Button4_Click(object sender, RoutedEventArgs e)
-        {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "4";
-            }
-            else
-            {
-                outbox.Text += "4";
-            }
-        }
-
-        private void Button5_Click(object sender, RoutedEventArgs e)
-        {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "5";
-            }
-            else
-            {
-                outbox.Text += "5";
-            }
-        }
-
-        private void Button6_Click(object sender, RoutedEventArgs e)
-        {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "6";
-            }
-            else
-            {
-                outbox.Text += "6";
-            }
-        }
-
-        private void Button7_Click(object sender, RoutedEventArgs e)
-        {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "7";
-            }
-            else
-            {
-                outbox.Text += "7";
-            }
-        }
-
-        private void Button8_Click(object sender, RoutedEventArgs e)
-        {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "8";
-            }
-            else
-            {
-                outbox.Text += "8";
-            }
-        }
-
-        private void Button9_Click(object sender, RoutedEventArgs e)
-        {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "9";
-            }
-            else
-            {
-                outbox.Text += "9";
-            }
-        }
-
-        private void ButtonDot_Click(object sender, RoutedEventArgs e)
-        {
-            if (outbox.Text == "0" || outbox.Text == "" || outbox.Text == null)
-            {
-                outbox.Text = "0,";
-            }
-            else
-            {
-                outbox.Text += ",";
-            }
-        }
-
         private void ButtonPlus_Click(object sender, RoutedEventArgs e)
         {
             num1 = float.Parse(outbox.Text);
             outbox.Clear();
-            outbox.Focus();
             operation = 1;
         }
-
+        
         private void ButtonMinus_Click(object sender, RoutedEventArgs e)
         {
             if(outbox.Text != "")
             {
                 num1 = float.Parse(outbox.Text);
                 outbox.Clear();
-                outbox.Focus();
                 operation = 2;
             }
         }
@@ -183,7 +63,6 @@ namespace LabSiedem.WpfApp
         {
             num1 = float.Parse(outbox.Text);
             outbox.Clear();
-            outbox.Focus();
             operation = 3;
         }
 
@@ -191,33 +70,33 @@ namespace LabSiedem.WpfApp
         {
             num1 = float.Parse(outbox.Text);
             outbox.Clear();
-            outbox.Focus();
             operation = 4;
         }
 
         private void ButtonEquals_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == 4)
+            switch (operation)
             {
-                result = num1 * float.Parse(outbox.Text);
-                outbox.Text = result.ToString();
-            }
-            else if (operation == 3)
-            {
-                result = num1 / float.Parse(outbox.Text);
-                outbox.Text = result.ToString();
-            }
-            else if (operation == 2)
-            {
-                result = num1 - float.Parse(outbox.Text);
-                outbox.Text = result.ToString();
-            }
-            else if (operation == 1)
-            {
-                result = num1 + float.Parse(outbox.Text);
-                outbox.Text = result.ToString();
-            }
+                case 1:
+                    result = num1 + float.Parse(outbox.Text);
+                    outbox.Text = result.ToString();
+                    break;
 
+                case 2:
+                    result = num1 - float.Parse(outbox.Text);
+                    outbox.Text = result.ToString();
+                    break;
+
+                case 3:
+                    result = num1 / float.Parse(outbox.Text);
+                    outbox.Text = result.ToString();
+                    break;
+
+                case 4:
+                    result = num1 * float.Parse(outbox.Text);
+                    outbox.Text = result.ToString();
+                    break;
+            }
         }
 
         private void ButtonClear_Click(object sender, RoutedEventArgs e)

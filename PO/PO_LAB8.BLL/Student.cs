99be8b66e4bec36e34f76c90ad3d88e20db21d93
Dio_Brando <Dio_brando@gmail.com>
2024-04-24 17:27:44 +0200

    LAB8 simple database

---
+++ PO/PO_LAB8.BLL/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO_LAB8.BLL
+{
+    public class Student
+    {
+        public string FirstName {  get; set; }
+        public string LastName {  get; set; }
+        public string Faculty {  get; set; }
+        public int Index {  get; set; }
+        public Student(string FName, string LName, string fac, int id)
+        {
+            FirstName = FName;
+            LastName = LName;
+            Faculty = fac;
+            Index = id;
+        }
+    }
+}
---
+++ PO/PO_LAB8/TabelaStud.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PO_LAB8.BLL;
+
+namespace PO_LAB8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class TabelaStud : Window
+    {
+        public IList<Student> Students { get; set; }
+        public TabelaStud()
+        {
+            Students = new List<Student>() { new Student("Heorhii", "Mikula", "WIMII", 228666) };
+            InitializeComponent();
+            DataGridTable.Columns.Add(item: new DataGridTextColumn() {Header = "FirstName", Binding = new Binding(path: "FirstName") });
+            DataGridTable.Columns.Add(item: new DataGridTextColumn() { Header = "LastName", Binding = new Binding(path: "LastName") });
+            DataGridTable.Columns.Add(item: new DataGridTextColumn() { Header = "Faculty", Binding = new Binding(path: "Faculty") });
+            DataGridTable.Columns.Add(item: new DataGridTextColumn() { Header = "Index", Binding = new Binding(path: "Index") });
+
+            DataGridTable.AutoGenerateColumns = false;
+            DataGridTable.ItemsSource = Students;
+
+
+        }
+    }
+}

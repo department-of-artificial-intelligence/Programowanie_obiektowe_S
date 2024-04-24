9a180566b47085f24b5a88f661f3f5d9c6e21c2d
Mateusz_Kulik <smth@smth.smth>
2024-04-24 16:47:40 +0200

    Po.Lab8

---
+++ PO/Po.Lab8/Lab8/Lab8.WpfApp/DodawanieStudenta.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Lab8.BLL;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Lab8.WpfApp
+{
+    public partial class DodawanieStudenta : Window
+    {
+        public IList<Student> Students { get; set; }
+        public DodawanieStudenta()
+        {
+            InitializeComponent();
+
+            Students = new List<Student>
+            {
+                new Student() {FirstName="Jan",SurName="Kowal",Faculty="WXW",StudentNo=1 },
+                new Student() {FirstName="Anna",SurName="Nowak",Faculty="WXW",StudentNo=2 },
+            };
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "First name", Binding = new Binding("FirstName") });
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "Sur name", Binding = new Binding("SurName") });
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "Faculty", Binding = new Binding("Faculty") });
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "Student No.", Binding = new Binding("StudentNo") });
+            DataGridStudents.AutoGenerateColumns = false;
+            DataGridStudents.ItemsSource = Students;
+        }
+    }
+}

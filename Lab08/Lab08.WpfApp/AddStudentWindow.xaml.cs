a2780abeb294cdf7bdceea19ced2465a6f401170
Jakub Moskalik <jakub_moskali@pc.pl>
2024-04-24 17:46:43 +0200

    lab8

---
+++ Lab08/Lab08.WpfApp/AddStudentWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Lab08.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab08.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for AddStudentWindow.xaml
+    /// </summary>
+    public partial class AddStudentWindow : Window
+    {
+        public Student Student { get; set; }
+        public AddStudentWindow()
+        {
+            InitializeComponent();
+            if (student != null) 
+            {
+                TextBoxFaculty.Text = student.Faculty;
+                TextBoxFirstName.Text = student.FirstName;
+                TextBoxStudentNo.Text = student.StudentNo.ToString();
+                TextBoxSurName.Text = Student.SurName;
+            }
+            Student = Student ?? new Student();
+        }
+        private void ButtonAddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            if(!Regex.IsMatch(TextBoxFirstName.Text, @"^\p{L}{1,12}$") ||
+               !Regex.IsMatch(TextBoxSurName.Text, @"^\p{L}{1,12}$") ||
+               !Regex.IsMatch(TextBoxFaculty.Text, @"^\p{L}{1,12}$") ||
+               !Regex.IsMatch(TextBoxFirstName.Text, @"^[0-9]{4,10}$"))
+            {
+                MessageBox.Show("Invalid input data");
+                return;
+            }
+            Student.FirstName = TextBoxFirstName.Text;
+
+        }
+
+
+    }
+}
---
+++ Lab08/Lab08.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Lab08.BLL;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab08.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IList<Student> Students { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Students = new List<Student>
+            {
+                new Student() {FirstName = "Jan", SurName = "Kowalski", Faculty = "WIMII", StudentNo = 1010},
+                new Student() {FirstName = "Michał", SurName = "Nowak", Faculty = "WIMII", StudentNo = 1011},
+                new Student() {FirstName = "Jacek", SurName = "Makieta", Faculty = "WIMII", StudentNo = 1012},
+            };
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "FirstName", Binding = new Binding("FirstName") });
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "SurName", Binding = new Binding("SurName") });
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "Faculty", Binding = new Binding("Faculty") });
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "FirstName", Binding = new Binding("Student No.") });
+            DataGridStudents.Columns.Add(new DataGridTextColumn() { Header = "Grades", Binding = new Binding("Grades") });
+            DataGridStudents.AutoGenerateColumns = false;
+            DataGridStudents.ItemsSource = Students;
+        }
+
+        private void DataGridStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}

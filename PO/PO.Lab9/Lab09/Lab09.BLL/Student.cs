d52e5ed8562be0f3b5bda51222911cf075c1b6ee
Student <Student@LAB514-14>
2024-04-26 18:15:52 +0200

    lab9.start

---
+++ PO/PO.Lab9/Lab09/Lab09.BLL/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09.BLL
+{
+    public class Student
+    {
+        private string _firstName;
+        private string _surName;
+        private string _faculty;
+        private int _studentNo;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string SurName
+        {
+            get { return _surName; }
+            set { _surName = value; }
+        }
+        public string Faculty
+        {
+            get { return _faculty; }
+            set { _faculty = value; }
+        }
+        public int StudentNo
+        {
+            get { return _studentNo; }
+            set { _studentNo = value; }
+        }
+
+        public Student()
+        {
+            _firstName = string.Empty;
+            _surName = string.Empty;
+            _faculty = string.Empty;
+            _studentNo = 0;
+        }
+
+        public Student(string firstName, string surName, string faculty, int studentNo)
+        {
+            _firstName = firstName;
+            _surName = surName;
+            _faculty = faculty;
+            _studentNo = studentNo;
+        }
+    }
+}
---
+++ PO/PO.Lab9/Lab09/Lab09.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Lab09.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab09.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IList<Student> Students { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Students = new List<Student>()
+            {
+                new Student(){FirstName = "Jan", SurName = "Kowalski", Faculty = "WIMII", StudentNo = 1010},
+                new Student(){FirstName = "Michał", SurName = "Nowak", Faculty = "WIMII", StudentNo = 1011},
+                new Student(){FirstName = "Jacek", SurName = "Makieta", Faculty = "WIMII", StudentNo = 1012}
+            };
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "First name", Binding = new Binding(path: "FirstName") });
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "Sur name", Binding = new Binding(path: "SurName") });
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "Faculty", Binding = new Binding(path: "Facultu") });
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "Student No.", Binding = new Binding(path: "StudentNo") });
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "Grades", Binding = new Binding(path: "JoinedGrades") });
+            DataGridStudents.AutoGenerateColumns = false;
+            DataGridStudents.ItemsSource = Students;
+
+        }
+
+        private void DataGridStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}

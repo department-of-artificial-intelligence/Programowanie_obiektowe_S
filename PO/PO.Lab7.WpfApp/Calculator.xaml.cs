4b522578819a3952e3fc6ac9e02bb4a5cfbd4e11
Mikołaj Taborek <smth@smth.smth>
2024-05-06 14:08:07 +0200

    ads

--- PO/PO.Lab7.WpfApp/Calculator.xaml.cs
+++ PO/PO.Lab7.WpfApp/Calculator.xaml.cs
@@ -12,77 +12,17 @@ namespace PO.Lab7.WpfApp
             InitializeComponent();
         }
 
-        private void Button5_click(object sender, RoutedEventArgs e)
-        {
-            Result.Text = Content.ToString();
-        }
-
-        private void Button4_click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void Button3_click(object sender, RoutedEventArgs e)
+        private void Button_math(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void Button2_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_1_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ButtonSum_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ButtonDifference_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ButtonProduct_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ButtonQuotient_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ButtonResult_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button0_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button9_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button8_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button7_click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button6_click(object sender, RoutedEventArgs e)
+        private void Button_equal(object sender, RoutedEventArgs e)
         {
 
         }

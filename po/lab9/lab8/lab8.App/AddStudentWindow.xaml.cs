c326f31ccdabf24874fef4ca843e3b57b9de5cf4
Milosz_Mostek <losowy@gmail.com>
2024-05-15 15:48:04 +0200

    lab9

---
+++ po/lab9/lab8/lab8.App/AddStudentWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Lab8.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace lab8.App
+{
+    /// <summary>
+    /// Interaction logic for AddStudentWindow.xaml
+    /// </summary>
+    public partial class AddStudentWindow : Window
+    {
+        public Student Student { get; set; }
+        public AddStudentWindow(Student? student = null)
+        {
+            InitializeComponent();
+            if (Student != null)
+            {
+                TextBoxFaculty.Text = student.Faculty;
+                TextBoxFirstName.Text = student.FirstName;
+                TextBoxStudentNo.Text = student.StudentNo.ToString();
+                TextBoxSurName.Text = student.SurName;
+            }
+            Student = student ?? new Student();
+
+
+        }
+        private void ButtonAddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Regex.IsMatch(input: TextBoxFirstName.Text, pattern: @"^\p{L}{1,12}$") ||
+            !Regex.IsMatch(input: TextBoxSurName.Text, pattern: @"^\p{L}{1,12}$"))
+            {
+                MessageBox.Show(messageBoxText: "Invalid input data");
+                return;
+            }
+            Student.FirstName = TextBoxFirstName.Text;
+            Student.SurName = TextBoxSurName.Text;
+            Student.Faculty = TextBoxFaculty.Text;
+            if (!int.TryParse(TextBoxStudentNo.Text, out int studentNo))
+                MessageBox.Show(messageBoxText: "Student is not a number.");
+            Student.StudentNo = studentNo;
+            DialogResult = true;
+        }
+
+       
+    }
+}
---
+++ po/lab9/lab8/lab8.App/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Lab8.BLL;
+
+namespace lab8.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IList<Student> Students { get; set; }
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            Students = new List<Student>()
+            {
+                new Student(){FirstName = "Jan", SurName = "Kowalski", Faculty = "WIMI", StudentNo = 1010},
+                new Student(){FirstName = "Michal", SurName = "Nowak", Faculty = "WIMI", StudentNo = 1011},
+                new Student(){FirstName = "Jacek", SurName = "Makieta", Faculty = "WIMI", StudentNo = 1012},
+            };
+
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "FirstName", Binding = new Binding(path: "FirstName") });
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "SurName", Binding = new Binding(path: "SurName") });
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "Faculty", Binding = new Binding(path: "Faculty") });
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "StudentNo", Binding = new Binding(path: "StudentNo") });
+            DataGridStudents.AutoGenerateColumns = false;
+            DataGridStudents.ItemsSource = Students;
+
+        }
+
+        private void ButtonRemoveStudentWindowShow_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGridStudents.SelectedItem is Student studentToRemove)
+            {
+                Students.Remove(studentToRemove);
+                DataGridStudents.Items.Refresh();
+            }
+        }
+
+        private void ButtonAddStudentWindowShow_Click(object sender, RoutedEventArgs e)
+        {
+            AddStudentWindow asw = new AddStudentWindow();
+
+            if (asw.ShowDialog().Value)
+            {
+                Students.Add(asw.Student);
+                DataGridStudents.Items.Refresh();
+            }
+        }
+
+        private void SaveToTxt_Click(object sender, RoutedEventArgs e)
+        {
+            FileStream fs = new FileStream("data.txt", FileMode.Create);
+            StreamWriter sw = new  StreamWriter(fs);
+
+            sw.WriteLine("[[Student]]");
+            sw.Close();
+        }
+    }
+}

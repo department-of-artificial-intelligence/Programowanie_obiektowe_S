abdbf9f43d1e176bd2e395e53b960db6b9a4d9e8
Jakub Grzybowski <dfvfd>
2024-05-22 01:54:00 +0200

    vn

---
+++ Aplikacje WPF/Lab08/Student/StudentMainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Student.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Student.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for StudentMainWindow.xaml
+    /// </summary>
+    public partial class StudentMainWindow : Window
+    {
+        public IList<BLL.Student> Students { get; set; }
+        public StudentMainWindow()
+        {
+
+            InitializeComponent();
+            Students = new List<BLL.Student>
+            {
+                new BLL.Student(){FirstName = "Jan" , LastName = "Kowaki" , Faculity = "WIMII" , Id = 135248 },
+                new BLL.Student(){FirstName = "Michal" , LastName = "Nowak" , Faculity = "WIMII" , Id = 135963 },
+                new BLL.Student(){FirstName = "Adam" , LastName = "Spiner" , Faculity = "WIMII" , Id = 135987 }
+            };
+        }
+
+
+        private void REMOVE(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ADD(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

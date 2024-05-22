6b79c346b7c272db2bb60d0fab3cba3a5c7f6c12
Aleksander_Piekarz <smth@smth.pl>
2024-05-22 15:49:08 +0200

    Test

---
+++ PO/Po.lab10/Lab10.Model/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public long StudentIndex { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string SurName { get; set; } = null!;
+        public string Faculty { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+        //[Hide]
+        public IList<Grade> Grades { get; set; } = null!; // właściwość nawigacyjna
+        public string AllGrades => Grades.Count > 0 ? string.Join(", ", Grades) : "";
+    }
+}

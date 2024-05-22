ddfc719399e4152eba6bf46719c94211e24e45aa
Yaraslau_Pazniak <GRAAYSIT@Gmail.com>
2024-05-22 15:42:59 +0200

    #LAB10

---
+++ Lab10/Lab10.DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+//using Lab11.Model.Entities;
+using Lab11.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace Lab11.DAL.EF;
+public class ApplicationDbContext : DbContext
+{
+    public virtual DbSet<Grade> Grades { get; set; } = null!;
+    public virtual DbSet<Student> Students { get; set; } = null!;
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+}

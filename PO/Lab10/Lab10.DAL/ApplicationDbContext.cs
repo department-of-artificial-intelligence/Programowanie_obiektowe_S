18e722e318baaeb0c49a49f784b2d06b35facbd6
Oskar_Jagielski <msth@msth.pl>
2024-05-22 15:52:20 +0200

    Lab10

---
+++ PO/Lab10/Lab10.DAL/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab10.Model;
+
+namespace Lab10.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<Grade> Grades { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}

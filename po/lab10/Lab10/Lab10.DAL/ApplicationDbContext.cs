f256aae91ca615e3411221364ba4156c78e96784
Milosz_Mostek <losowy@gmail.com>
2024-05-22 15:45:53 +0200

    lab10

---
+++ po/lab10/Lab10/Lab10.DAL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+
+using Lab10.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Lab10.DAL.EF;
+public class ApplicationDbContext : DbContext
+{
+    public virtual DbSet<Grade> Grades { get; set; } = null!;
+    public virtual DbSet<Student> Students { get; set; } = null!;
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+}
---
+++ po/lab10/Lab10/Lab10.Model/Student.cs
@@ -0,0 +1,24 @@
+﻿using Lab10.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Lab10.Model.Attributes;
+using System.Globalization;
+
+namespace Lab10.Model.Entities;
+
+    public class Student
+        {
+            public int Id { get; set; }
+            public long StudentIndex { get; set; }
+            public string FirstName { get; set; } = null!;
+            public string SurName { get; set; } = null!;
+            public string Faculty { get; set; } = null!;
+            public DateTime DateOfBirth { get; set; }
+            //[Hide]
+            public IList<Grade> Grades { get; set; } = null!; // właściwość nawigacyjna
+            public string AllGrades => Grades.Count > 0 ? string.Join(", ", Grades) : "";
+        }
+

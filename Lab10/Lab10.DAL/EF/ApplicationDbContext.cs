e6734462e25268b0dfd8aba89a3ba670e4762abd
Vladdjuga <55984362+Vladdjuga@users.noreply.github.com>
2024-05-28 20:18:20 +0200

    aaa

---
+++ Lab10/Lab10.DAL/EF/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Lab10.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10.DAL.EF
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<Grade> Grades { get; set; } = null!; public virtual DbSet<Student> Students { get; set; } = null!;
+        public ApplicationDbContext() { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Vlad_Dzhuha_PO_AppDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ Lab10/Lab10.Model/Entities/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10.Model.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public long StudentIndex { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string SurName { get; set; } = null!;
+        public string Faculty { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+        public IList<Grade> Grades { get; set; } = null!; // właściwość nawigacyjna
+        public string AllGrades => Grades.Count > 0 ? string.Join(", ", Grades) : "";
+    }
+}

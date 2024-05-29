bc75aba1d5dbca929ab62e48b575562adb3e0672
Student <Student@LAB-515>
2024-05-29 15:33:53 +0200

    ssss

--- Po/Lab10.DAL/ApplicationDbContext.cs
+++ Po/Lab10.DAL/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace Lab10.DAL
 {
     public class ApplicationDbContext : DbContext
     {
+        
         public virtual DbSet<Grade> Grades { get; set; } = null!;
         public virtual DbSet<Student> Students { get; set; } = null!;
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)

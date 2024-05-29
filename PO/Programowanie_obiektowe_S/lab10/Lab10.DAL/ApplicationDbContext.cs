aaa9847cbc24f34a4a8784d9d267f8f280380411
Artem_Grun <qwer@qwer.com>
2024-05-29 17:29:35 +0200

    error

---
+++ PO/Programowanie_obiektowe_S/lab10/Lab10.DAL/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Lab10.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+namespace Lab10.DAL.EF;
+public class ApplicationDbContext : DbContext
+{
+    public virtual DbSet<Grade> Grades { get; set; } = null!;
+    public virtual DbSet<Student> Students { get; set; } = null!;
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+}
+
---
+++ PO/Programowanie_obiektowe_S/lab10/Lab10.Model/Grade.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+namespace Lab10.Model.Entities;
+public class Grade
+{
+    //[Key] // Adnotacja klucza głownego.
+    public int Id { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
+    public string Subject { get; set; } = null!;
+    public double Value { get; set; }
+    //// Dawniej wymagana adnotacja klucza obcego,
+    //// parametr name musi być zgodna z nazwą własciwości nawigacyjnej
+    //[ForeignKey("Student")]
+    //public int StudentId { get; set; } // właściwość klucza obcego
+    public Student Student { get; set; } = null!; //właściwość nawigacyjna
+    public override string ToString()
+    {
+        return Value.ToString(CultureInfo.InvariantCulture);
+    }
+}

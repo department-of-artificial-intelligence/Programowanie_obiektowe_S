b12d1516c19b07ed9179c75a145e2870736b68c5
Viacheslav Shevchenko <zxcslavik@slav.com>
2024-05-29 17:49:21 +0200

    lab10

---
+++ Lab10/Lab10.Model/Grade.cs
@@ -0,0 +1,19 @@
+using System.Globalization;
+namespace Lab10.Model.Entities;
+public class Grade
+{
+    //[Key] // Adnotacja klucza g³ownego.
+    public int Id { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
+    public string Subject { get; set; } = null!;
+    public double Value { get; set; }
+    //// Dawniej wymagana adnotacja klucza obcego,
+    //// parametr name musi byæ zgodna z nazw¹ w³asciwoœci nawigacyjnej
+    //[ForeignKey("Student")]
+    //public int StudentId { get; set; } // w³aœciwoœæ klucza obcego
+    public Student Student { get; set; } = null!; //w³aœciwoœæ nawigacyjna
+    public override string ToString()
+    {
+        return Value.ToString(CultureInfo.InvariantCulture);
+    }
+}

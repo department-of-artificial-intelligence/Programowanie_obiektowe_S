8555a31b436622c4af1704ebe232bf733f31164c
Jakub Moskalik <jakub_moskali@pc.pl>
2024-05-29 17:40:29 +0200

    s

---
+++ lab10/lab10.DAL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using lab10.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace lab10.DAL;
+public class ApplicationDbContext : DbContext
+{
+    public virtual DbSet<Grade> Grades { get; set; } = null!;
+    public virtual DbSet<Student> Students { get; set; } = null!;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+    {
+    }
+}
---
+++ lab10/lab10.Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using lab10.Model.Attributes;
+namespace lab10.Model;
+public class Student
+{
+    public int Id { get; set; }
+    public long StudentIndex { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string SurName { get; set; } = null!;
+    public string Faculty { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; }
+    [Hide]
+    public IList<Grade> Grades { get; set; } = null!; // właściwość nawigacyjna
+    public string AllGrades => Grades.Count > 0 ? string.Join(", ", Grades) : "";
+}

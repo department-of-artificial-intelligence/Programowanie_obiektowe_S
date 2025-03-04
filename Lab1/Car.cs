4af723a4b5ccc52df903b958a2d07141522a5ed1
Maksym_Klym <smth@smth.smth>
2025-03-04 13:45:08 +0100

    Lab1 start

---
+++ Lab1/Car.cs
@@ -0,0 +1,31 @@
+﻿namespace Lab1
+{
+    public class Car
+    {
+        public string brand { get; set; }
+        public string model { get; set; }
+        public int doorCount { get; set; }
+        public float engineVolume { get; set; }
+        public double avgConsump { get; set; }
+        public int carCount { get; set; }
+
+        public Car()
+        {
+            brand = "";
+            model = "";
+            doorCount = 0;
+            engineVolume = 0;
+            avgConsump = 0;
+        }
+
+        public Car(string brand, string model, int doorCount, float engineVolume, double avgConsump, int carCount)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.doorCount = doorCount;
+            this.engineVolume = engineVolume;
+            this.avgConsump = avgConsump;
+            this.carCount = carCount;
+        }
+    }
+}

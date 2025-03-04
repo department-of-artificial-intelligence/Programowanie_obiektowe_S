5dfe3cd87deaa69ca3c7b43327d84f62e8fa382e
Daksym_Domilovskyi <smth@smth.smth>
2025-03-04 13:48:55 +0100

    Zadanie 1 Wykonane

--- Lab1/Car.cs
+++ Lab1/Car.cs
@@ -1,49 +1,87 @@
 ﻿using System;
 
-namespace Lab1
+public class Car
 {
-    public class Car
+    private string _brand;
+    private string _model;
+    private int _doorCount;
+    private double _engineVolume;
+    private double _avgConsump;
+
+    private static int _carCount = 0;
+
+    public Car()
     {
-        private string _brand;
-        private string _model;
-        private int _doorCount;
-        private double _engineVolume;
-        private double _avgConsump;
+        _brand = "Unknown";
+        _model = "Unknown";
+        _doorCount = 0;
+        _engineVolume = 0.0;
+        _avgConsump = 0.0;
+        _carCount++;
+    }
 
-        private static int _carCount = 0;
+    public Car(string brand, string model, int doorCount, double engineVolume, double avgConsump)
+    {
+        _brand = brand;
+        _model = model;
+        _doorCount = doorCount;
+        _engineVolume = engineVolume;
+        _avgConsump = avgConsump;
+        _carCount++;
+    }
 
-        public Car()
-        {
-            _brand = "Unknown";
-            _model = "Unknown";
-            _doorCount = 0;
-            _engineVolume = 0.0;
-            _avgConsump = 0.0;
-            _carCount++;
-        }
-        public Car(string brand, string model, int doorCount, double engineVolume, double avgConsump)
-        {
-            _brand = brand;
-            _model = model;
-            _doorCount = doorCount;
-            _engineVolume = engineVolume;
-            _avgConsump = avgConsump;
-            _carCount++;
-        }
-        public string Brand
-        {
-            get { return _brand; }
-            set { _brand = value; }
-        }
-        public string Model
-        {
-            get { return _model; }
-            set { _model = value; }
-        }
-        public int DoorCount
-        {
-            get { return _doorCount; }
-            set { _doorCount = value; }
-        }
+    public string Brand
+    {
+        get { return _brand; }
+        set { _brand = value; }
+    }
+
+    public string Model
+    {
+        get { return _model; }
+        set { _model = value; }
+    }
+
+    public int DoorCount
+    {
+        get { return _doorCount; }
+        set { _doorCount = value; }
+    }
+
+    public double EngineVolume
+    {
+        get { return _engineVolume; }
+        set { _engineVolume = value; }
+    }
+
+    public double AvgConsump
+    {
+        get { return _avgConsump; }
+        set { _avgConsump = value; }
+    }
+
+    public double CalculateConsump(double distance)
+    {
+        return (_avgConsump * distance) / 100;
+    }
+
+    public double CalculateCost(double distance, double fuelPricePerLiter)
+    {
+        double consumption = CalculateConsump(distance);
+        return consumption * fuelPricePerLiter;
+    }
+
+    public void DisplayCarInfo()
+    {
+        Console.WriteLine($"Brand: {_brand}");
+        Console.WriteLine($"Model: {_model}");
+        Console.WriteLine($"Door Count: {_doorCount}");
+        Console.WriteLine($"Engine Volume: {_engineVolume}L");
+        Console.WriteLine($"Average Consumption: {_avgConsump}L/100km");
+    }
+
+    public static void DisplayCarCount()
+    {
+        Console.WriteLine($"Number of cars created: {_carCount}");
     }
 }
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,1 +1,26 @@
 ﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Car("Toyota", "Corolla", 4, 1.8, 6.5);
+        Car car2 = new Car("Ford", "Mustang", 2, 5.0, 12.0);
+
+        car1.DisplayCarInfo();
+        Console.WriteLine();
+        car2.DisplayCarInfo();
+        Console.WriteLine();
+
+        double distance = 300;
+        double fuelPricePerLiter = 5.5;
+
+        double consumption = car1.CalculateConsump(distance);
+        double cost = car1.CalculateCost(distance, fuelPricePerLiter);
+
+        Console.WriteLine($"Car 1 consumption for {distance}km: {consumption}L");
+        Console.WriteLine($"Car 1 fuel cost for {distance}km: {cost} zł");
+        Console.WriteLine();
+
+        Car.DisplayCarCount();
+    }
+}

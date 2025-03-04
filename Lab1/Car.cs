565c36cdb91a4a98f845cff35c67729060e9a8bf
Michal_Lukasik <smth@smth.com>
2025-03-04 13:45:07 +0100

    Lab 1 start

---
+++ Lab1/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private float _engineVolume;
+        private double _avgConsump;
+
+        public string Brand { get { return _brand; } }
+
+
+
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab1
+{
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}

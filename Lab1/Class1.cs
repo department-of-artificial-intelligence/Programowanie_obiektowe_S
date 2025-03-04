cda3b9baaf5dee7b33491fd4f0766e72b0d884f7
Jakub_Blada <rafik@Wasik.onet.pl>
2025-03-04 15:36:04 +0100

    Lab1 Start

---
+++ Lab1/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private float _engineVolume;
+        private double _avgConsump;
+        private string _registrationNumber;
+
+        private static int _carCount = 0;
+
+        public Car()
+        {
+            _brand = "nieznana";
+            _model = "nieznany";
+            _doorCount = 0;
+            _engineVolume = 0.0f;
+            _avgConsump = 0.0;
+            _registrationNumber = "nieznany";
+            _carCount++;
+        }
+
+        public Car(string brand, string model, int doorCount, float engineVolume, double avgConsump, string registrationNumber)
+        {
+            _brand = brand;
+            _model = model;
+            _doorCount = doorCount;
+            _engineVolume = engineVolume;
+            _avgConsump = avgConsump;
+            _registrationNumber = registrationNumber;
+            _carCount++;
+        }
+
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int DoorCount { get; set; }
+        public float EngineVolume { get; set; }
+        public double AvgConsump
+        {
+            get { return _avgConsump; }
+            set { _avgConsump = value; }
+        }
+        public string RegistrationNumber
+        {
+            get { return _registrationNumber; }
+            set { _registrationNumber = value; }
+        }
+        public double CalculateConsump(double roadLength)
+        {
+            return (_avgConsump * roadLength) / 100;
+        }
+        public double CalculateCost(double roadLength, double petrolCost)
+        {
+            double consum = CalculateConsump(roadLength);
+            return consum * petrolCost;
+        }
+        public override string ToString()
+        {
+            return $"Car | Brand: {_brand}, Model: {_model}, NumOfDoors: {_doorCount}, EngineVol: {_engineVolume}, AvgConsump:{_avgConsump}, RegistrationNumber: {_registrationNumber}";
+        }
+        public void Details()
+        {
+            Console.WriteLine(this.ToString());
+        }
+        public static void DisplayCarCount()
+        {
+            Console.WriteLine($"Liczba utworzonych obiektów Car: {_carCount}");
+        }
+    }
+}

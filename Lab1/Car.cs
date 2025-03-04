5e97fd55c6a32647017091206ca02de995de10d9
Lukasz_Zych <example@example.com>
2025-03-04 22:51:36 +0100

    Lab 1 Start (Sync test) + default constructors

--- Lab1/Car.cs
+++ Lab1/Car.cs
@@ -13,12 +13,47 @@ namespace Lab1
         private int _doorCount;
         private float _engineVolume;
         private double _avgConsump;
-        private static int carCount;
+        private static int carCount = 0;
+
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set { _doorCount = value; }
+        }
+
+        public float EngineVolume
+        {
+            get { return _engineVolume; }
+            set { _engineVolume = value; }
+        }
 
         public double AvgConsump
         {
             get { return _avgConsump; }
             set { _avgConsump = value; }
+        }
+
+        Car()
+        {
+            _brand = "nieznana";
+            _model = "nieznany";
+            _doorCount = 0;
+            _engineVolume = 0;
+            _avgConsump = 0.0;
+            carCount++;
+
         }
     }
 }

b39b06f91637bb0cad999dc12836e47f2456d1a0
GabrielSwiergolik <RandomMail123321@gmail.com>
2025-03-04 15:42:39 +0100

    lab1

---
+++ Lab1/Car.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private float _engineVolume;
+        private double _avgConsump;
+        private static int _carCount = 0;
+        private string _registrationNumber;
+        
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set { _doorCount = value; }
+        }
+
+        public float EngineVolume
+        {
+            get { return _engineVolume; }
+            set { _engineVolume = value; }
+        }
+
+        public double AvgConsump
+        {
+            get { return _avgConsump; }
+            set { _avgConsump = value; }
+        }
+
+        public string RegistrationNumber
+        {
+            get { return _registrationNumber; }
+            set { _registrationNumber = value; }
+        }
+
+        public static int CarCount => _carCount;
+
+        public Car()
+        {
+            _brand = "nieznana";
+            _model = "nieznany";
+            _doorCount = 0;
+            _engineVolume = 0.0f;
+            _avgConsump = 0.0d;
+            _registrationNumber = "nieznany";
+            _carCount++;
+        }
+
+        public Car (string brand, string model, int doorCount, float engineVolume, double avgConsump, string registrationNumber)
+        {
+            _brand = brand;
+            _model = model;
+            _doorCount = doorCount; 
+            _engineVolume = engineVolume;
+            _avgConsump = avgConsump;
+            _registrationNumber = registrationNumber;
+            _carCount++;
+        }
+
+        public double CalculateConsump(double roadLenght)
+        {
+            return (_avgConsump * roadLenght) / 100;
+        }
+
+        public double CalculateCost(double roadLenght, double petrolCost)
+        {
+            return CalculateConsump(roadLenght) * petrolCost;
+        }
+
+        public override string ToString()
+        {
+            return $"Car | Brand: {_brand}, Model: {_model}, NumOfDoors:{ _doorCount}, EngineVol: { _engineVolume}, AvgConsump:{ _avgConsump}, RegistrationNumber: {_registrationNumber}";
+        }
+
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+
+        public static void DisplayCarCount()
+        {
+            Console.WriteLine(_carCount);
+        }
+
+
+    }
+
+    public class Garage
+    {
+        private Car[] _cars;
+        private string _address;
+    }
+
+}

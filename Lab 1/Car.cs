c542aa127f5334c4213971304c0f29e32f51cf47
Vladyslav Tkachenko <VladTkachenko123@gmail.com>
2025-03-04 15:45:33 +0100

    Lab 1 start

---
+++ Lab 1/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+   public class Car
+    {
+        private string _brand;
+        private string _marka;
+        private int _doorCount;
+        private float _engineVolume;
+        private double _avgConsump;
+        private static int _carCount = 0;
+
+        public string Brand { get; set; }
+        public string Marka { get; set; }
+        public int DoorCount { get; set; }
+        public float EngineVolume { get; set; }
+        public double avgConsump { get; set; }
+
+        public Car()
+        {
+            _brand = string.Empty;
+            _marka = string.Empty;
+            _doorCount = 0;
+            _engineVolume = 0;
+            _avgConsump = 0;
+            _carCount++; 
+
+        }
+
+        public Car(string brand, string marka, int doorCount, float engineVolume, double avgConsump)
+        {
+            _brand = brand;
+            _marka = marka;
+            _doorCount = doorCount;
+            _engineVolume = engineVolume;
+            _avgConsump = avgConsump;
+            _carCount++;
+            
+        }
+
+        public double CalculateConsump(double roadLength)
+        {
+            
+            return (roadLength/100) * _avgConsump;
+        }
+        public double CalculateCost(double roadLength,double petrolCost) 
+        {
+            return CalculateConsump(roadLength) * petrolCost;        
+        }
+
+        public void Details()
+        {
+            Console.WriteLine()
+        }
+    }
+}
+
---
+++ Lab 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

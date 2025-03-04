96d2afafef4f2a440f2156e2cd1523fc7e32f419
Szymon Kupczyk <smth@smth.smth>
2025-03-04 13:45:06 +0100

    Lab1 start

---
+++ Lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Lab1/car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private float _engineVolume;
+        private double _avgComsump;
+        private static int _carCount;
+
+        public string Brand { get; set;}
+        public string Model { get; set;}
+        public int DoorCount { get; set;}
+        public float EngineVolume { get; set;}
+        public double AverageComsump { get; set; }
+    }
+
+}

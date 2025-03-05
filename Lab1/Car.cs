36e564e4e217cdaacf671e0f22daa82fd6363626
Kacper Gutkowski <fakemail@fake.mail>
2025-03-05 13:48:19 +0100

    Lab 1 zad 1

---
+++ Lab1/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+	public class Car
+	{
+		private string _brand;
+		private string _model;
+		private int _doorCount;
+		private float _engineVolume;
+		private double _avgConsump;
+		private static int _carCount=0;
+
+		public string Brand
+		{
+			get { return _brand; }
+			set { _brand = value; }
+		}
+		public string Model
+		{
+			get { return _model; }
+			set { _model = value; }
+		}
+
+		public int DoorCount
+		{
+			get { return _doorCount; }
+			set { _doorCount = value; }
+		}
+
+		public float EngineVolume
+		{
+			get { return _engineVolume; }
+			set { _engineVolume = value; }
+		}
+		public double AvgConsump
+		{
+			get { return _avgConsump; }
+			set { _avgConsump = value; }
+		}
+		public int CarCount
+		{
+			get { return _carCount; }
+		}
+
+		public Car()
+		{
+			_brand = "nieznana";
+			_model = "nieznany";
+			_doorCount = 0;
+			_engineVolume = 0;
+			_avgConsump = 0.0;
+
+		}
+
+	}
+}
+
+//Zmieniamy internal class na public class
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,10 +4,34 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+
+			Console.WriteLine("He~llo World!");
 		}
 	}
 }
 
 // Ctrl shift B aby tylko skompilować bez uruchamiania
-// Pierwszy commit 
+// Pierwszy commit "Lab 1 start" wybieramy commit all and sync i klikamy strzałkę w górę aby zrobić push do online
+//Jak komuś sie nie udało skomitować to od instrukcji 20 w pdfie gita
+//W domy okienko z tokenem wyskoczy, kopiujemy personal access token z moodla i sign in
+
+//Nazwa funkji to adres w pamięci
+//parametry to jest to co przyjmuje funkcja, argumenty to jest co jest w momencie wywołania
+
+//Po zdaniu kolokwium plusy się liczą, plusy nic nie znaczą jeśli nie zdasz kolokwium
+
+//Różnica między const and read only. readonly to tylko modyfikator który ustala dane pole jako tylko do odczytu, ale zapis jego może być w konstruktorze i może przechowywac typy referencyjne jak klasy. Jego wartość jest znana w runtime a nie w compilation time jak static
+//Właściwośc wygląda jak pole ale zachowuje się jak metoda
+//Nikt poważny po Polsku nie pisze
+//Konwencje .Net, jak class to nazwa z wielkiej litery "Obiekt", pola w klasach z podłogą "_pole"
+// => jak return || "get {return _area;}" to samo "get => _area"
+// automatycznie w compile time "public int DoorCount{get; set;}" ale to są domyślne set i get bez naszych zmian
+//W c++ jak skompilujemy pod windowsem wezmiemy execa i damy na linuksa to nie zadziała
+//W c# program uruchamia się na wirtualnej maszynie .Net dzięki temu mogę tą samą binarkę przenieść między systemami (Win/Lin) i zadziała
+//CIL(IL) - Common Intermediate Language --> CLR
+//W C# niejawna klasa Object po której wszystkie klasy dziedziczą
+//Pierwsze kolokwium UML i możliwości zaimplementowania 
+//Relacje między klasami musi byc znane w nocy o północy. Agregacja, kompozycja itp.
+//Zadanie stworzenie klasy Car
+//ctr shift s lub file ->save all
+//Git changes

c902e833c1e8ea409287966c5b3de4c95c6b893f
Kacper Gutkowski <fakemail@fake.mail>
2025-03-05 12:46:00 +0100

    Lab1 start

---
+++ Lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
+
+// Ctrl shift B aby tylko skompilować bez uruchamiania
+// Pierwszy commit 

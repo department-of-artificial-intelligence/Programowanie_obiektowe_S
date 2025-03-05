e1c4204ec5e0b178e26e68509fc6d90f5c799b7a
Agnieszka_Labuzinska <ls@ls.com>
2025-03-05 13:48:19 +0100

    Lab1 zad1

---
+++ Labb1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Car
+    {
+        private:
+        string _brand;
+        string _model;
+        int _doorCount;
+        float _engineVolume;
+        double _avgConsump;
+        int _carCount;
+
+
+
+
+    }
+}

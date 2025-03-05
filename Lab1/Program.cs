2a71337a727002ec8c1191e45bb14261b693931a
Roman_Melnyk <random123@smth.smth>
2025-03-05 12:45:59 +0100

    Lab1 start

---
+++ Lab1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Security.Cryptography;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("HELLO");
+        }
+    }
+}

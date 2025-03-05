16f0c491031a758c0b297cf83e45b6bcfdc1b410
Nazar_Koretskyi <smth@smth.smth>
2025-03-05 13:48:20 +0100

    Lab 1 zad 1

---
+++ Lab1/Car.cs
@@ -0,0 +1,50 @@
+﻿namespace Lab1
+{
+    public class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private float _engineVolume;
+        private double _avgConsump;
+        private int _carCount;
+
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set { _doorCount = value; }
+        }
+        public float EngineVolume
+        {
+            get { return _engineVolume; }
+            set { _engineVolume = value; }
+        }
+        public double AvgConsump
+        {
+            get { return _avgConsump; }
+            set { _avgConsump = value; }
+        }
+        public int CarCount
+        {
+            get { return _carCount; }
+            set { _carCount = value; }
+        }
+        public Car()
+        {
+            _engineVolume = 0;
+            _carCount++;
+        }
+        //public Car(string _brand,string _model,int )
+
+    }
+}

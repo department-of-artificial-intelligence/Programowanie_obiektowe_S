0ac22b40c9a35d72e33e35ca553505f510ef9413
Roman_Melnyk <random123@smth.smth>
2025-03-05 13:48:19 +0100

    Lab1 zad1 end

---
+++ Lab1/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Car
+    {
+        public string _brand;
+        public string _model;
+        private int _doorCount;
+        private float _engineVolume;
+        private double _avgConsump;
+        private static int _carCount = 0;
+        public string brand {get; set;}
+        public string model { get; set;}
+        public int doorCount { get; set;}
+        public float engineVolume { get; set;}
+        public double avgConsump 
+        {
+         get { return _avgConsump; }
+         set { _avgConsump = value; }
+        }
+
+        Constructor()
+        {
+            brand = null;
+            model = null;
+
+        }
+
+
+    }
+}

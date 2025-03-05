d2c0491f045494fe93e6e899812e75edbc8734a0
Aleksandra_Papis <ap@ap.com>
2025-03-05 13:48:18 +0100

    lab1 zad1

---
+++ Lab1/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private float _engineCount;
+        private double _avgConsump;
+        private static int _carCount=0;
+
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set { _doorCount = value; }
+        }
+        public float EngineCount
+        {
+            get { return _engineCount; }
+            set { _engineCount = value; }
+        }
+        public double AvgConsump
+        {
+            get { return _avgConsump; }
+            set { _avgConsump = value; }
+        }
+
+    }
+}

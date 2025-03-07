027086165e8298debf2515e093fccdf2c7c44f9b
Back420 <150706876+Back420@users.noreply.github.com>
2025-03-07 09:17:34 +0100

    Car class implemented Garage class implemented test code added

--- Lab1/Car.cs
+++ Lab1/Car.cs
@@ -14,37 +14,86 @@ namespace Lab1
         private int _doorCount;
         private float _engineVolume;
         private double _avgConsump;
+        private string _registrationNumber;
         private static int _carCount;
 
-        public string Brand { get; set; }
-        public string Model { get; set; }
-        public int DoorCount { get; set; }
-        public float EngineVolume { get; set; }
-        public double avgConsump { get; set; }
+        public string Brand 
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set { _doorCount = value; }
+        }
+        public float EngineVolume
+        {
+            get { return _engineVolume; }
+            set { _engineVolume = value; }
+        }
+        public double AvgConsump
+        {
+            get { return _avgConsump; }
+            set { _avgConsump = value; }
+        }
+
+        public string RegistrationNumber
+        {
+            get { return _registrationNumber; }
+            set { _registrationNumber = value; }
+        }
 
         public Car()
         {
-            _brand = "none";
-            _model = "none";
+            _brand = "nieznana";
+            _model = "nieznany";
             _doorCount = 0;
-            _engineVolume = 0f;
-            _avgConsump = 0f;
+            _engineVolume = 0.0f;
+            _avgConsump = 0.0;
+            _registrationNumber = "nieznana";
             _carCount++;
         }
 
-        public Car(string brand, string model, int doorCount, float engineVolume, double avgConsump)
+        public Car(string brand, string model, int doorCount, float engineVolume, double avgConsump, string registrationNumber)
         {
             _brand = brand;
             _model = model;
             _doorCount = doorCount;
             _engineVolume = engineVolume;
             _avgConsump = avgConsump;
+            _registrationNumber = registrationNumber;
             _carCount++;
         }
 
+        public double CalculateConsump(double roadLength) 
+        {
+            return (_avgConsump * roadLength) / 100.0;
+        }
+
+        public double CalculateCost(double roadLength, double petrolCost)
+        {
+            return CalculateConsump(roadLength) * petrolCost;
+        }
         public override string ToString()
         {
-            return $"{_brand} {_model}";
+            return $"Car | Brand: {_brand}, Model: {_model}, " +
+                $"NumOfDoors: {_doorCount}, EngineVol: {_engineVolume}, " +
+                $"AvgConsump: {_avgConsump}, RegistrationNumber: {_registrationNumber}"; 
+        }
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+
+        public static void DisplayCarCount()
+        {
+            Console.WriteLine(_carCount);
         }
     }
 }
---
+++ Lab1/Garage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Garage
+    {
+        private string _address;
+        private int _carsCount = 0;
+        private int _capacity;
+        private Car[] _cars;
+
+        public string Address 
+        {
+            get { return _address; }
+            set { _address = value; } 
+        }
+        public int Capacity
+        {
+            get { return _capacity; }
+            set { _capacity = value;
+                  _cars = new Car[value]; }
+        }
+
+        public Garage()
+        {
+            _address = "nieznany";
+            //_carsCount = 0;
+            _capacity = 0;
+            _cars = null;
+        }
+        public Garage(string address, int capacity)
+        {
+            _address = address;
+            _capacity = capacity;
+            _cars = new Car[_capacity];
+        }
+
+        public void CarIn(Car car)
+        {
+            if(_carsCount >= _capacity)
+            {
+                Console.WriteLine("The garage is full!");
+            }
+            else
+            {
+                _cars[_carsCount] = car;
+                _carsCount++;
+            }
+        }
+
+        public Car CarOut()
+        {
+            if(_carsCount == 0)
+            {
+                Console.WriteLine("The garage is empty!");
+                return null;
+            }
+            else
+            {
+                _carsCount--;
+                Car Temp = _cars[_carsCount];
+                _cars[_carsCount] = null;
+                return Temp;
+            }
+        }
+
+        public override string ToString()
+        {
+            string temp = $"Garage | Address: {_address}, CarsCount : {_carsCount}, Capacity : {_capacity} \nCars:\n";
+            foreach (Car car in _cars)
+            {
+                temp = temp + car + "\n";
+            }
+            return temp;
+        }
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+
+
+    }
+}

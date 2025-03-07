3b4fb9c8b86ce19cb8034e64b058182a83e1d619
Piotr_Krzysinski <sth@sth.sth>
2025-03-07 19:49:26 +0100

    Garage, Car, Program

--- Lab1/Car.cs
+++ Lab1/Car.cs
@@ -62,12 +62,12 @@ namespace Lab1
 
         public Car(string? brand, string? model, int doorCount, float engineVolume, double avgConsump, string? registrationNumber)
         {
-            Brand = brand;
-            Model = model;
-            DoorCount = doorCount;
-            EngineVolume = engineVolume;
-            AvgConsump = avgConsump;
-            RegistrationNumber = registrationNumber;
+            _brand = brand;
+            _model = model;
+            _doorCount = doorCount;
+            _engineVolume = engineVolume;
+            _avgConsump = avgConsump;
+            _registrationNumber = registrationNumber;
             ++_carCount;
         }
 
@@ -95,28 +95,6 @@ namespace Lab1
         public static void DisplayCarCount()
         {
             Console.WriteLine($"There are {_carCount} cars.");
-        }
-
-        static void Main()
-        {
-            Car car1 = new Car();
-            car1.Details();
-            car1.Brand = "Fiat";
-            car1.Model = "126p";
-            car1.DoorCount = 2;
-            car1.EngineVolume = 650;
-            car1.AvgConsump = 6.0;
-            car1.RegistrationNumber = "KR12345";
-            car1.Details();
-            Car car2 = new Car("Syrena", "105", 2, 0.8f, 7.6d, "WE1234");
-            car2.Details();
-            Console.WriteLine(car1);
-            double routeConsumption = car2.CalculateConsump(500);
-            Console.WriteLine($"Route consumption: {routeConsumption} l");
-            double routeCost = car2.CalculateCost(500, 5);
-            Console.WriteLine($"Route cost: {routeCost}");
-            Car.DisplayCarCount();
-            Console.WriteLine("\r\n=========================================\r\n");
         }
     }
 }
--- Lab1/Garage.cs
+++ Lab1/Garage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -30,7 +31,78 @@ namespace Lab1
 
         public Garage()
         {
+            _address = null;
+            _capacity = 0;
+            _cars = null;
+        }
 
+        public Garage(string? address, int capacity)
+        {
+            _address = address;
+            _capacity = capacity;
+            _cars = new Car[capacity];
+        }
+
+        public void CarIn(Car c)
+        {
+            if(_carsCount >= _capacity)
+            {
+                Console.WriteLine("Garage is full. Cannot add the car.");
+            }
+            else
+            {
+                _cars[_carsCount] = c;
+                ++_carsCount;
+                Console.WriteLine($"Car {c.Brand} {c.Model} has been added to the garage.");
+            }
+        }
+
+        public Car CarOut()
+        {
+            if(_carsCount==0)
+            {
+                Console.WriteLine("The garage is empty.");
+                return null!;
+            }
+            else
+            {
+                Car carToRemove = _cars[_carsCount - 1];
+                _cars[_carsCount - 1] = null;
+                --_carsCount;
+                Console.WriteLine($"Car {carToRemove.Brand} {carToRemove.Model} has been removed from the garage.");
+                return carToRemove;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Garage Address: {_address ?? "Unknown"}");
+            result.AppendLine($"Capacity: {_capacity}");
+            result.AppendLine($"Cars count: {_carsCount}");
+
+            if(_carsCount>0)
+            {
+                result.AppendLine("Cars in the garage:");
+                foreach(Car car in _cars)
+                {
+                    if(car != null)
+                    {
+                        result.AppendLine(car.ToString());
+                    }
+                }
+            }
+            else
+            {
+                result.AppendLine("No cars in the garage.");
+            }
+
+            return result.ToString();
+        }
+
+        public void Details()
+        {
+            Console.WriteLine(this);
         }
     }
 }

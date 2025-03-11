c3d7e8fe1bcde170754ee7fb340c8940ea8b0229
Vitalii Ponomarenko <Student@LAB-515>
2025-03-11 13:57:33 +0100

    start aa

---
+++ zas31/student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace lab3
+{
+	public class Student
+	{
+		public int year { get; set; }
+		public int group { get; set; }
+		public string id { get; set; }
+		public Student() { }
+		Student(int year, int group, string id)
+		{
+			this.year=year;
+			this.group=group;
+			this.id=id;
+		}
+		public override string ToString()
+		{
+			return $"brooo";
+		}
+		public void Details()
+		{
+			Console.WriteLine(this);
+		}
+	}
+}

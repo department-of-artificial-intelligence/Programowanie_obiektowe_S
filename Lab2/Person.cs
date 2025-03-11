6e7201468e08933144ffdda827a8f03bfe9ac23e
Jakub <nieIsteniejacy@mail.mail>
2025-03-11 13:44:30 +0100

    Labolatorium 2

---
+++ Lab2/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Person
+    {
+
+        protected string _firstName;
+        protected string _lastName;
+        protected DateTime _dateOfBirth;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+
+        public Person()
+        {
+            _firstName = string.Empty;
+            _lastName = string.Empty;
+            _dateOfBirth = DateTime.MinValue;
+        }
+
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _dateOfBirth = dateOfBirth;
+        }
+        public override string ToString()
+        {
+            return $", firstName: {_firstName}, lastName: {_lastName}, dateOfBirth: {_dateOfBirth}";
+        }
+
+        public virtual void Details()
+        {
+            Console.WriteLine("Person | " + ToString());
+        }
+    }
+}
---
+++ Lab2/Player.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Player : Person
+    {
+        private string _position;
+        private string _club;
+        private int _scoredGoals;
+
+        public string Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+        public string Club
+        {
+            get { return _club; }
+            set { _club = value; }
+        }
+
+        public int ScoredGoals
+        {
+            get { return _scoredGoals; }
+            set { _scoredGoals = value; }
+        }
+
+        public Player() : base()
+        {
+            _position = string.Empty;
+            _club = string.Empty;
+            _scoredGoals = 0;
+        }
+
+        public Player(string firstName,  string lastName, DateTime dateOfBirth, string position, string club, int scoredGoals):base(firstName, lastName,dateOfBirth)
+        {
+            _position = position;
+            _club = club;
+            _scoredGoals = scoredGoals;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", position: {_position}, club: {_club}, scored goals: {_scoredGoals}";
+        }
+
+        public override void Details()
+        {
+            Console.WriteLine("Player | " + ToString());
+        }
+
+        public void ScoreGoal()
+        {
+            _scoredGoals++;
+        }
+    }
+
+
+}

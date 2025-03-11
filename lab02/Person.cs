3876cd7ddc6596f4fe85d5eeb0eb04773d0f649a
Malgorzata Czx <mc@mc.pl>
2025-03-11 15:43:40 +0100

    tak

---
+++ lab02/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02
+{
+    public class Person
+    {
+        protected string _firstName;
+        protected string _lastName;
+        protected DateTime _dateOfBirth;
+
+
+        public string FirstName { 
+            get { return _firstName; } 
+            set { _firstName = value; } 
+        }
+
+        public string LastName { 
+            get { return _lastName; }
+            set { _lastName = value;  }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+
+        public Person()
+        {
+            _firstName = string.Empty;
+            _lastName = string.Empty;
+            _dateOfBirth = DateTime.MinValue;
+        }
+
+        public Person( string firstName, string lastName, DateTime dateOfBirth)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _dateOfBirth = dateOfBirth;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Imie: {_firstName}, nazwisko: {_lastName}, data urodzenia: {_dateOfBirth}. ";
+        }
+
+        public virtual void Details()
+        {
+            Console.WriteLine(this);        }
+
+    }
+    }
---
+++ lab02/Player.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02
+{
+    public class Player: Person{
+        
+        private string _position;
+        private string _club;
+        public int _scoredGoals;
+
+        public string Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public string Club
+        {
+            get { return _club; }
+            set { _club = value; }
+        }
+
+        public int ScoredGoals
+        {
+            get { return _scoredGoals;}
+            set { _scoredGoals = value;}
+        }
+
+        public Player():base()
+        {
+            _position = string.Empty;
+            _club = string.Empty;
+            _scoredGoals = 0;
+        }
+
+        public Player(string firstName, string lastName, DateTime dateOfBirth, string position, string club, int scoredGoals):base(firstName,lastName,dateOfBirth)
+        {
+            _position = position;
+            _club = club;
+            _scoredGoals=scoredGoals;
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString() + $" pozycja: {_position}, klub: {_club}, ilosc goli: {_scoredGoals}"; 
+                }        
+
+
+        public void ScoreGoal()
+        {
+            _scoredGoals++;
+        }
+
+
+    }
+}

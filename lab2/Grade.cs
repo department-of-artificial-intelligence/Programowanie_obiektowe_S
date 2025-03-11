b5a476d985be870f45abbd8ac546d4002cf5efd8
Wiktor_Wisniewski <wiktor@korpo.pl>
2025-03-11 15:44:38 +0100

    lab2

---
+++ lab2/Grade.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2 {
+	internal class Grade {
+		private string _subjectName;
+		private double _value;
+		private DateTime _date;
+
+		public Grade() {
+		}
+
+		public Grade(string subjectName, double value, DateTime date) {
+			_subjectName = subjectName;
+			_date = date;
+			_value = value;
+		}
+
+		public string SubjectName {
+			get { return _subjectName; }
+			set { _subjectName = value; }
+		}
+		public DateTime Date {
+			get { return _date; }
+			set { _date = value; }
+		}
+
+		public double Value {
+			get { return _value; }
+			set { _value = value; }
+		}
+
+		public override string ToString() {
+			return
+				   $"nazwa przedmiotu: {this._subjectName}, " +
+				   $"data: {this.Date} " +
+				   $"wartosc: {this.Value}";
+		}
+
+		public void Details() {
+			Console.WriteLine(this);
+		}
+
+	}
+}
---
+++ lab2/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2 {
+	internal class Person {
+		protected string _firstName;
+		protected string _lastName;
+		protected DateTime _dateOfBirth;
+
+		public Person() { }
+
+		public Person(string firstName, string lastName, DateTime dateOfBirth) {
+			_firstName = firstName;
+			_lastName = lastName;
+			_dateOfBirth = dateOfBirth;
+		}
+
+		public string FirstName {
+			get => _firstName;
+			set => _firstName = value;
+		}
+
+		public string LastName {
+			get => _lastName;
+			set => _lastName = value;
+		}
+
+		public DateTime DateOfBirth {
+			get => _dateOfBirth;
+			set => _dateOfBirth = value;
+		}
+
+		public override string ToString() {
+			return $"imie: {this.FirstName}, " +
+				   $"nazwisko: {this.LastName}, " +
+				   $"data urodzin: {this.DateOfBirth.ToString("dd-MM-yyyy")}";
+		}
+
+		public virtual void Details() {
+			Console.WriteLine(this);
+		}
+
+	}
+}

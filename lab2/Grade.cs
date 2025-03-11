96e3dbed393bc971b51c455d09e8fd8d5b628e00
Maciej_Pecina <smth@smth.smth>
2025-03-11 13:48:26 +0100

    lab02 - 2

---
+++ lab2/Grade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Grade 
+    {
+        protected string _subjectName;
+        protected DateTime _date;
+        protected 
+    }
+}

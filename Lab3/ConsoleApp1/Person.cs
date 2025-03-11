d057200f7d2c20736ac547f236be852dce314b8f
Vladyslav Tkachenko <VladTkachenko123@gmail.com>
2025-03-11 15:48:21 +0100

    Lab 3

---
+++ Lab3/ConsoleApp1/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private DateTime _dateOfBirth;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return DateOfBirth; }
+            set { DateOfBirth = value; }
+        }
+
+        public Person()
+        {
+            _firstName = string.Empty;
+            _lastName = string.Empty;
+            _dateOfBirth = DateTime.MinValue;
+        }
+
+        public Person(string firstName , string lastName, DateTime dateOfBirth)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _dateOfBirth = dateOfBirth;
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Person | FirstName: {_firstName}, LastName: {_lastName}, DateOfBirth: {_dateOfBirth}";
+        }
+
+        public virtual void Details()
+        {
+            Console.WriteLine(this);
+        }
+    }
+}
---
+++ Lab3/ConsoleApp1/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Player : Person
+    {
+        private string _position;
+        private string _club;
+        private int _scoreGoals;
+
+        public string Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public string Club
+        {
+            get { return _club; }
+            set { _club = value; }
+        }
+
+        public int ScoreGoals
+        {
+            get { return _scoreGoals; }
+            set { _scoreGoals = value; }
+        }
+
+        public Player()
+            :base()
+        {
+            _position = string.Empty;
+            _club = string.Empty;
+            _scoreGoals = 0;
+        }
+
+        public Player(string firstName, string lastName, DateTime dateOfBirth,string position,string club,int scoreGoals)
+            : base(firstName, lastName, dateOfBirth)
+        {
+            _position=position;
+            _club=club;
+            _scoreGoals=scoreGoals;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Player | Position: {_position}, Club: {_club}, ScoreGoals: {_scoreGoals}";
+        }
+
+        public void ScoreGoal()
+        {
+            _scoreGoals++;
+        }
+    }
+}

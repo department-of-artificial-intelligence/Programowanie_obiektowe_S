dd769dd815b80d7d53ac2418fe250345d5879c70
Vitalii Ponomarenko <Student@LAB-515>
2025-03-11 13:42:44 +0100

    lab3 start

---
+++ Lab1/persona.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using lab31;
+using lab3;
+namespace labmain
+{
+	public class Person
+	{
+		protected string _firstName = "";
+		protected string _lastName = "";
+		protected DateTime _birthDate;
+
+		public string firstName
+		{
+			get { return _firstName; }
+			set { _firstName = value; }
+		}
+		public string lastName
+		{
+			get { return _lastName; }
+			set { _lastName = value; }
+		}
+		public DateTime birthDate
+		{
+			get { return _birthDate; }
+			set { _birthDate = value; }
+		}
+		public Person() { }
+		public Person(string firstName, string lastName, DateTime birthDate)
+		{
+			_firstName=firstName;
+			_lastName=lastName;
+			_birthDate=birthDate;
+			_firstName=firstName;
+			_lastName=lastName;
+			_birthDate=birthDate;
+		}
+		public override string ToString()
+		{
+			return $"brooo";
+		}
+
+		public void Details()
+		{
+			Console.WriteLine(this);
+		}
+	}
+	
+}
---
+++ Lab1/player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab31 { 
+	class Player
+	{
+		protected string _position;
+		protected string _club;
+		protected int _scoredGoals;
+	}
+	public string position
+	public Player(string position, string club, int scoredGoals)
+	{
+
+	}
+	public override string ToString()
+	{
+		return $"goaaaal";
+	}
+
+	public void ScoreGoal()
+	{
+		Console.WriteLine(this);
+	}
+}
+
+

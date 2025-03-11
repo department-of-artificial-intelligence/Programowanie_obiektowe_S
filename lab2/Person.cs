fe56411de68449e94f2f9e4245ad591057b02199
Artem Bovtun <artembovtun312321@gmail.com>
2025-03-11 15:46:50 +0100

    lab 2

---
+++ lab2/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private DateTime _dateOfBirth;
+
+        public string FirstName { 
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+        public Person()
+        {
+            _firstName = string.Empty;
+            _lastName = string.Empty;
+            _dateOfBirth = DateTime.MinValue;
+        }
+        public Person(string firstName, string lastName, DateTime dateTime)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _dateOfBirth = dateTime;
+        }
+        public override string ToString()
+        {
+            return $"Person | FirstName: {_firstName}, _lastName: {_lastName}, DateOfBirth: {_dateOfBirth}";
+        }
+
+        public virtual void Details()
+        {
+            Console.WriteLine(this);
+        }
+    }
+}
---
+++ lab2/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    internal class Student : Person
+    {
+        private int _year;
+        private int _group;
+        private int _indexID;
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+        public int Group
+        {
+            get { return _group; }
+            set { _group = value; }
+        }
+
+
+        public int IndexID
+        {
+            get { return _indexID; }
+            set { _indexID = value; }
+        }
+        public Student()
+            :base()
+        {
+            _year = 0;
+            _group = 0;
+            _indexID = 0;
+        }
+        public Student(string firstName, string lastName, DateTime dateTime, int year, int group, int indexId):
+            base(firstName, lastName, dateTime)
+        {
+            _year = year;
+            _group = group;
+            _indexID = indexId;
+        }
+        public override string ToString()
+        { 
+            return base.ToString() + $" Student | Year: {_year}, Group: {_group}, IndexID: {_indexID}"; 
+        }
+    }
+}

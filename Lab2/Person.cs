3d9997a2a63e75e53d23187a6da663732380d7a6
Piotr_Krzysinski <smth@smth.smth>
2025-03-12 13:02:41 +0100

    lab2 start

---
+++ Lab2/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Person
+    {
+        protected required string _firstName;
+        protected required string _lastName;
+        protected required DateTime _dateOfBirth;
+
+        public string FirstName { 
+            get => _firstName; set => _firstName = value;
+        } 
+        public string LastName {
+            get => _lastName; set => _lastName = value;
+        }
+        public DateTime DateOfBirth { 
+            get => _dateOfBirth; set => _dateOfBirth = value;
+        }
+
+        public Person()
+        {
+            _firstName = "none";
+            _lastName = "none";
+            _dateOfBirth = default;
+        }
+
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _dateOfBirth = dateOfBirth;
+        }
+
+        public override string ToString() => $"{FirstName} {LastName} {DateOfBirth}";
+
+
+    }
+}

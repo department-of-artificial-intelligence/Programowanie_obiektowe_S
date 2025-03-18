819a3957ff6fe99285b20086d129ba81da34e177
Daksym_Domilovskyi <smth@smth.smth>
2025-03-18 12:32:07 +0100

    lab2 wykonane

---
+++ Lab2/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2 {
+	internal class Player : Person {
+		private string _position;
+		private string _club;
+		private int _scoredGoals;
+
+		public Player() {
+		}
+
+		public Player(string firstName, string lastName, DateTime dateOfBirth, string position, string club, int scoredGoals)
+			: base(firstName, lastName, dateOfBirth) {
+			_position = position;
+			_club = club;
+			_scoredGoals = scoredGoals;
+		}
+
+		public string Position {
+			get => _position;
+			set => _position = value;
+		}
+
+		public string Club {
+			get => _club;
+			set => _club = value;
+		}
+
+		public int ScoredGoals {
+			get => _scoredGoals;
+			set => _scoredGoals = value;
+		}
+
+		public override string ToString() {
+			return base.ToString() +
+				   $" pozycja: {this.Position}, " +
+				   $"klub: {this.Club}, " +
+				   $"zdobyte punkty {this.ScoredGoals}";
+		}
+
+		public void ScoreGoal() {
+			this.ScoredGoals += 1;
+		}
+
+	}
+}
---
+++ Lab2/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2 {
+	internal class Student : Person {
+		private int _year;
+		private int _group;
+		private int _indexId;
+		private List<Grade> _grades;
+
+		public Student() {
+		}
+
+		public Student(string firstName, string lastName, DateTime dateOfBirth, int year, int group, int indexId)
+				: base(firstName, lastName, dateOfBirth) {
+			_year = year;
+			_group = group;
+			_indexId = indexId;
+			this._grades = new List<Grade>();
+		}
+
+		public int Year {
+			get { return _year; }
+			set { _year = value; }
+		}
+
+		public int Group {
+			get { return _group; }
+			set { _group = value; }
+		}
+
+		public int IndexId {
+			get { return _indexId; }
+			set { _indexId = value; }
+		}
+
+		public List<Grade> Grades {
+			get => this._grades;
+		}
+
+		public void AddGrade(string subjectName, double value, DateTime date) {
+			this.Grades.Add(new Grade(subjectName, value, date));
+		}
+
+		public void AddGrade(Grade grade) {
+			this.Grades.Add(new Grade(grade.SubjectName, grade.Value, grade.Date));
+		}
+
+		public override string ToString() {
+			string napis = base.ToString() + " " +
+				   $"rok: {this.Year}, " +
+				   $"grupa: {this.Group}, " +
+				   $"index id: {this.IndexId} " +
+				   $"oceny: ";
+
+			this.Grades.ForEach(grade => napis = napis + grade.ToString() + '\n');
+
+			return napis;
+		}
+
+	}
+}

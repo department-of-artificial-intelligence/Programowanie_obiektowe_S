d6b0fb57a9fcc977f34b895d7428647f7925212d
Maciej_Pęcina <sth@sth.sth>
2025-03-18 10:50:43 +0100

    lab2 zad2

--- lab2/Grade.cs
+++ lab2/Grade.cs
@@ -8,8 +8,51 @@ namespace lab2
 {
     public class Grade 
     {
-        protected string _subjectName;
-        protected DateTime _date;
-        protected 
+        private string _subjectName;
+        private DateTime _date;
+        private double _value;
+
+        public Grade()
+        {
+            _subjectName = "brak";
+            _date = default(DateTime);
+            _value = 0.0;
+        }
+
+        public Grade(string subjectName, double value, DateTime date)
+        {
+            _subjectName = subjectName;
+            _date = date;
+            _value = value;
+        }
+
+        public string SubjectName
+        {
+            get { return _subjectName; }
+            set { _subjectName = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public double Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                    $"Grade | SubjectName: {_subjectName}, Date: {_date}, Value: {_value}";
+        }
+
+        public void Details()
+        {
+            Console.WriteLine(this.ToString());
+        }
     }
 }
--- lab2/Student.cs
+++ lab2/Student.cs
@@ -11,16 +11,16 @@ namespace lab2
         private int _year;
         private int _group;
         private int _indexId;
+        private IList<Grade> _grades = new List<Grade>();
 
-        public Student() 
+        public Student()
         {
             _year = 0;
             _group = 0;
             _indexId = 0;
         }
 
-        public Student(string firstName, string lastName, DateTime dateOfBirth,
-                      int year, int group, int indexId) :
+        public Student(string firstName, string lastName, DateTime dateOfBirth, int year, int group, int indexId) :
                       base(firstName, lastName, dateOfBirth)
         {
             _year = year;
@@ -47,8 +47,67 @@ namespace lab2
 
         public override string ToString()
         {
-            return base.ToString() + 
+            return base.ToString() +
                     $"Student:| Year: {_year}, Group: {_group}, IndexId: {_indexId} ";
         }
+
+        public void AddGrade(string subjectName, double value, DateTime date)
+        {
+            _grades.Add(new Grade(subjectName, value, date));
+        }
+
+        public void AddGrade(Grade grade)
+        {
+            _grades.Add(grade);
+        }
+
+        public void DisplayGrades()
+        {
+            foreach(var grade in _grades)
+            {
+                grade.Details();
+            }
+        }
+
+
+        public void DisplayGrades(string subjectName)
+        {
+            foreach (var grade in _grades)
+            {
+                if (subjectName == grade.SubjectName)
+                {
+                    grade.Details();
+                }
+            }
+        }
+
+        public void DeleteGrade(string subjectName, double value, DateTime date)
+        {
+            _grades.Remove(new Grade(subjectName, value, date));
+        }
+
+        public void DeleteGrade(Grade grade)
+        {
+            _grades.Remove(grade);
+        }
+
+        public void DeleteGrades(string subjectName)
+        {
+            for (int i = _grades.Count - 1; i >= 0; i--)
+            {
+                if (_grades[i].SubjectName == subjectName)
+                {
+                    _grades.RemoveAt(i);
+                }
+            }
+            Console.WriteLine("Operacja zakonczona");
+        }
+
+        public void DeleteGrades()
+        {
+            _grades.Clear();
+        }
+        
+
     }
 }

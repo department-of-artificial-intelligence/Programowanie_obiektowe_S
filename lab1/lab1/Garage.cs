191b944ff89f67751fba1e4d6912dc2b27fc534e
agnieszkalabuzinska <agal@xxx>
2025-03-18 21:12:58 +0100

    lab1

---
+++ lab1/lab1/Garage.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_1
+{
+    public class Garage
+    {
+        private string _address;
+        private int _capacity;
+        private int _carsCount = 0;
+        private Car[] _cars;
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+        public int Capacity
+        {
+            get { return _capacity; }
+            set
+            {
+                _capacity = value;
+                _cars = new Car[value];
+            }
+        }
+
+        public Garage()
+        {
+            _address = "nieznany";
+            _capacity = 0;
+            _cars = null;
+        }
+
+        public Garage(string address, int capacity)
+        {
+            _address = address;
+            _capacity = capacity;
+            _cars = new Car[capacity];
+        }
+
+        public void CarIn(Car car)
+        {
+            if (_carsCount >= _capacity)
+            {
+                Console.WriteLine("Garaż zapełniony");
+            }
+
+            else
+            {
+                _cars[_carsCount] = car;
+                _carsCount++;
+                Console.WriteLine($"Samochód {car.Brand} dodany");
+            }
+        }
+
+        public Car CarOut()
+        {
+            if (_carsCount == 0)
+            {
+                Console.WriteLine($"Garaz jest pusty");
+                return null!;
+            }
+            else
+            {
+                Car removedCar = _cars[_carsCount - 1];
+                _cars[_carsCount - 1] = null;
+                _carsCount--;
+                Console.WriteLine($"Samochod {removedCar.Brand} zabrany z garażu");
+                return removedCar;
+            }
+        }
+        public override string ToString()
+        {
+            string carList = "Samochody w garażu: ";
+            foreach (var car in _cars)
+            {
+                if (car != null)
+                {
+                    carList += "\n" + car.ToString();
+                }
+            }
+            return $"Garaż | Adres: {_address}, Pojemność:{_capacity}, Liczba aut: {_carsCount} \n{carList}";
+        }
+
+        public void Details()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
---
+++ lab1/lab1/Person.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_1
+{
+    public class Person
+    {
+        private string[] _registrationNumber;
+        private int _carsCount;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int CarsCount { get => _carsCount; set => _carsCount = value; }
+        public int MaxCarCount { get; set; }
+
+        public Person()
+        {
+            FirstName = "nieznane";
+            LastName = "nieznane";
+            Address = "nieznany";
+            _carsCount = 0;
+            MaxCarCount = 3;
+            _registrationNumber = new string[MaxCarCount];
+        }
+        public Person(string firstName, string lastName, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            _carsCount = 0;
+            MaxCarCount = 3;
+            _registrationNumber = new string[MaxCarCount];
+        }
+        public Person(string firstName, string lastName, string address, Car[] cars)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            _carsCount = 0;
+            MaxCarCount = 3;
+            _registrationNumber = new string[MaxCarCount];
+
+            foreach (Car car in cars)
+            {
+                AddCarRegistrationNumber(car.RegistrationNumber);
+            }
+        }
+
+        public void AddCarRegistrationNumber(string registrationNumber)
+        {
+            if (_carsCount < MaxCarCount)
+            {
+                for (int i = 0; i < _registrationNumber.Length; ++i)
+                {
+                    if (_registrationNumber[i] == null)
+                    {
+                        _registrationNumber[i] = registrationNumber;
+                        _carsCount++;
+                        Console.WriteLine($"Car with registration " + registrationNumber + $" added");
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Registration list is full- new registration canot be added");
+            }
+        }
+
+        public void RemoveCarRegistrationNumber(string registrationNumber)
+        {
+            for (int i = 0; i < _registrationNumber.Length; ++i)
+            {
+                if (_registrationNumber[i] == registrationNumber)
+                {
+                    _registrationNumber[i] = null;
+                    _carsCount--;
+                    Console.WriteLine($"Car with registration " + registrationNumber + $" has been removed");
+                    return;
+                }
+            }
+            Console.WriteLine($"Cannot find car with registration {registrationNumber}");
+        }
+
+        public override string ToString()
+        {
+            string registrationList = "Registration numbers: ";
+            foreach (string registration in _registrationNumber)
+            {
+                if (registration != null)
+                {
+                    registrationList += "\n" + registration;
+                }
+            }
+            return $"Person: | First name {FirstName}, Last name {LastName}, Address {Address}, Cars: {registrationList}";
+        }
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+    }
+}
+

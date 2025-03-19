31a6a0f2f107cfb6c3ff2e4d83839d153346537b
Svatosik <sviatoslavilkiv05@gmail.com>
2025-03-19 13:42:15 +0100

    lab3

---
+++ Lab3_WJ/Catalog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_WJ
+{
+    public class Catalog
+    {
+        public IList<Item> Items { get; set; }
+        public string ThematicDepartment { get; set; }
+        public Catalog(IList<Item> items)
+        {
+            Items = items;
+            ThematicDepartment = "Brak";
+        }
+        public Catalog(string thematicDepartment, IList <Item> items)
+        {
+            Items = items;
+            ThematicDepartment = thematicDepartment;
+        }
+
+    }
+}
---
+++ Lab3_WJ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab3_WJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

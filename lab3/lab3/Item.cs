713b6c40cc84fbfebbee4543507123f3c2945f3f
Agnieszka_Labuzinska <ls@ls.com>
2025-03-19 13:44:04 +0100

    lab3

---
+++ lab3/lab3/Item.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public abstract class Item
+    {
+        protected int _id;
+        protected string _title;
+        protected string _publisher;
+        protected DateTime _dateOfIssue;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Title { get => _title; set => _title = value; }
+        public string Publisher { get => _publisher; set => _publisher = value; }
+        public DateTime DateOfIssue { get => _dateOfIssue; set => _dateOfIssue = value; }
+
+        public Item(string title, int id, string publisher, DateTime dateOfIssue)
+        {
+            _title = title;
+            _id = id;
+            _publisher = publisher;
+            _dateOfIssue = dateOfIssue;
+        }
+
+        public override string ToString()
+        {
+            string dateOfIssue = _dateOfIssue.ToString();
+            return $"Issue: | Title: {_title}, Id: {_id}, Publisher: {_publisher}, Date of issue: {dateOfIssue}";
+        }
+
+        public virtual void Details()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public abstract string GenerateBarCode()
+        {
+
+        }
+    }
+}
---
+++ lab3/lab3/Journal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public class Journal : Item
+    {
+        public int Number { get; set; }
+        public Journal(string title, int id, string publisher, DateTime dateOfIssue, int number) 
+            : base(title, id, publisher, dateOfIssue)
+        {
+            _title = title;
+            _id = id;
+            _publisher = publisher; 
+            _dateOfIssue = dateOfIssue;
+            _number = number;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Number {_number}";
+        }
+        public override string GenerateBarCode()
+        {
+            
+        }
+    }
+}

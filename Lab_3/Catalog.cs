7baed9c963c94bcc531eedf342d9fc3be03fb601
Nazar_Koretskyi <smth@smth.smth>
2025-03-19 13:47:58 +0100

    lab_3

---
+++ Lab_3/Catalog.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab_3
+{
+    internal class Catalog
+    {
+        public IList<Item> Items { get; set; } = new List<Item>();
+        public string ThematicDepartment { get; set; }
+        public Catalog(IList<Item> items, string thematicDepartment)
+        {
+            Items = items;
+            ThematicDepartment = thematicDepartment;
+        }
+
+    }
+
+}

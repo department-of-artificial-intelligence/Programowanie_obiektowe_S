a58d38e33b09ece723cb821f3864d2e2256c4943
Aleksandra_Papis <xbevz@OLA>
2025-03-21 17:21:23 +0100

    ??

---
+++ Lab1/Garage.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Garage
+    {
+        private string _address;
+        private int _capacity;
+        private int _carsCount=0;
+        private Car[] _cars;
+
+        public int Capacity
+        {
+            get { return _capacity; }
+            set { _capacity = value;
+                  _cars = new Car[_capacity];
+                }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+        public Garage()
+        {
+            _address = "none";
+            _capacity = 0;
+            _cars = new Car[_capacity];
+         }
+        public Garage(string address, int capacity)
+        {
+            _address = address;
+            _capacity = capacity;
+            _cars =new Car[capacity];
+        }
+        public void CarIn(Car car)
+        {
+            if (_carsCount >= _capacity)
+            {
+                Console.WriteLine("Garaz jest pelny!");
+            }
+            else
+            {
+                _cars[_carsCount] = car;
+                _carsCount++;
+                Console.WriteLine($"Dodano {car.Brand} {car.Model} do garazu.");
+            }
+        }
+        public Car CarOut()
+        {
+            if (_carsCount == 0)
+            {
+                Console.WriteLine("Garaz jest pusty!");
+                return null!;
+            }
+            else
+            {
+                Car wyprowadzone = _cars[_carsCount - 1];
+                _cars[_carsCount - 1] = null;
+                _carsCount--;
+                Console.WriteLine($"Wyprowadzono {wyprowadzone.Brand} {wyprowadzone.Model} z garazu");
+                return wyprowadzone;
+            }
+        }
+        public override string ToString()
+        {
+            string result = $"Garage | Address: {_address}, Capacity: {_capacity}, Cars Count: {_carsCount} \n";
+            
+            if(_carsCount > 0)
+            {
+                result += "Cars in the garage: ";
+                foreach (Car car in _cars)
+                {
+                    result += car.ToString() + "\n";
+                }
+            }
+            else
+            {
+                result += "There's no cars in the garage";
+            }
+
+                return result;
+        }
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+    }
+}
---
+++ Lab1/Person.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Person
+    {
+        private string[] _registrationNumbers;
+        private int _carsCount;
+        public int MaxCarCount { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+        public int CarsCount
+        {
+            get { return _carsCount; }
+            set { _carsCount = value; }
+        }
+
+        public void AddCarRegistrationNumber(string registrationNumber)
+        {
+            if (_carsCount >= MaxCarCount)
+            {
+                Console.WriteLine("Cant add another car, limit reached!");
+            }
+            else
+            {
+                _registrationNumbers[_carsCount] = registrationNumber;
+                _carsCount++;
+            }
+        }
+
+        public void RemoveCarRegistrationNumber(string registrationNumber)
+        {
+            if (_carsCount == 0)
+            {
+                Console.WriteLine("Theres no cars to remove!");
+            }
+            else
+            {
+                for (int i = 0; i < _carsCount; i++)
+                {
+                    if (_registrationNumbers[i] == registrationNumber)
+                    {
+                        _registrationNumbers[i] = null;
+                        _carsCount--;
+                        Console.WriteLine($"Car {registrationNumber} has been removed from {FirstName}{LastName}'s garage");
+                    }
+                }
+                Console.WriteLine($"Car with {registrationNumber} registration number not found");
+            }
+
+        }
+        public Person()
+        {
+            FirstName = "none";
+            LastName = "none";
+            Address = "none";
+            MaxCarCount = 3;
+            _carsCount = 0;
+            _registrationNumbers = new string[MaxCarCount];
+        }
+
+        public Person(string firstName, string lastName, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            MaxCarCount = 3;
+            CarsCount = 0;
+            _registrationNumbers = new string[MaxCarCount];
+        }
+
+        public Person(string firstName, string lastName, string address, Car[] cars)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            MaxCarCount = 3;
+            _registrationNumbers = new string[MaxCarCount];
+            _carsCount = 0;
+
+            foreach (Car car in cars)
+            {
+                AddCarRegistrationNumber(car.RegistrationNumber);
+            }
+        }
+        public override string ToString()
+        {
+            string result = $"Person | First name: {FirstName}, Last name: {LastName}, Address: {Address} \n";
+
+            if (_carsCount > 0)
+            {
+                result += "Cars: ";
+                for (int i = 0; i < _carsCount; i++)
+                {
+                    result += $"{_registrationNumbers[i]} \n";
+                }
+            }
+            return result;
+        }
+
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+
+    }
+}

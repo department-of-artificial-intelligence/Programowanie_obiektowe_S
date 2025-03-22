e7e3d3a3280063096cc3ab9dda3854cdac8f2370
Piotr Motyl <smth1@smth1.smth1>
2025-03-22 15:57:36 +0100

    lab3

---
+++ lab3/lab3/Author.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Author
+    {
+        protected string _firstName;
+        protected string _lastName;
+        protected string _nationality;
+
+        public string FirstName { get { return _firstName; } set { _firstName = value; } }
+        public string LastName { get { return _lastName; } set { _lastName = value; } }
+        public string Nationality { get { return _nationality; } set { _nationality = value; } }
+
+        public Author() {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Nationality = string.Empty;
+        }
+
+        public Author(string firstName, string lastName, string nationality)
+        {
+            
+            FirstName = firstName;
+            LastName = lastName;
+            Nationality = nationality;
+
+        }
+
+        public override string ToString()
+        {
+            return $"FirstName={FirstName},LastName={LastName},Nationality={Nationality}";
+        }
+    }
+}
---
+++ lab3/lab3/Journal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Journal :Item
+    {
+        protected int _number;
+
+        public int Number { get { return _number; } set { _number = value; } }
+                
+        public Journal() { }
+
+        public Journal(string title, int id, string publisher, DateTime dateOfIssue, int number):base(title,id,publisher,dateOfIssue)
+        {
+            Number = number;
+            
+        }
+
+        public override string ToString()
+        {
+
+            return $"Id={Id},Title={Title},Publisher={Publisher},DateOfIssue={DateOfIssue},Number={Number}";
+
+
+        }
+
+        public override string GenerateBarCode()
+        {
+            return $"J{Id}";
+        }
+    }
+}

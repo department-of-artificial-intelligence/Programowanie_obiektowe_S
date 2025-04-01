dc0ac3934c533c77704f2dab2e2dbd8d25bb2f13
smth <smth@smth.smth>
2025-04-01 13:44:05 +0200

    lab4 zad1

---
+++ lab4/lab4/OrgUnit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uni
+{
+    public class OrganizationUnit
+    {
+        public string Name { get; set; }
+        public string Adres { get; set; }
+        public List<Lecturer> Lecturers {get; set; }
+        public OrganizationUnit(string name, string adres, List<Lecturer> lecturers) {
+            Name = name;
+            Adres = adres;
+            Lecturers = lecturers;
+        }
+
+    }
+    
+}

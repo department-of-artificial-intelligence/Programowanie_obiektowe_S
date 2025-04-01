10d1b7b7b68b91c42195b71a2d58fa8c43686aed
Weronika_Jarzyna <sth.sth@sth.sth>
2025-04-01 20:28:13 +0200

    test

--- Lab1/Lab1/Car.cs
+++ Lab1/Lab1/Car.cs
@@ -6,7 +6,92 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
-    class Car
+    public class Car
     {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private double _engineVolume;
+        private double _avgConsump;
+        private string _registrationNumber;
+        private static int _carCount = 0;
+
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set { _doorCount = value; }
+        }
+        public double EngineVolume
+        {
+            get { return _engineVolume; }
+            set { _engineVolume = value; }
+        }
+        public double AvgConsump
+        {
+            get { return _avgConsump; }
+            set { _avgConsump = value; }
+        }
+        public string RegistrationNumber
+        {
+            get { return _registrationNumber; }
+            set { _registrationNumber = value; }
+        }
+
+        public Car()
+        {
+            _brand = "nieznana";
+            _model = "nieznany";
+            _doorCount = 0;
+            _engineVolume = 0.0;
+            _avgConsump = 0.0;
+            _registrationNumber = "nieznany";
+            _carCount++;
+        }
+
+        public Car(string brand, string model, int doorCount, double engineVolume, double avgConsump, string registrationNumber)
+        {
+            _brand = brand;
+            _model = model;
+            _doorCount = doorCount;
+            _engineVolume = engineVolume;
+            _avgConsump = avgConsump;
+            _registrationNumber = registrationNumber;
+            _carCount++;
+        }
+        public override string ToString()
+        {
+            return $"Car | Brand: {_brand}, Model: {_model}, NumOfDoors: {_doorCount}, EngineVol: {_engineVolume}, AvgConsump: {_avgConsump}, RegistrationNumber: {_registrationNumber}";
+        }
+
+        public double CalculateConsump(double roadLength)
+        {
+            return (_avgConsump * roadLength) / 100.0;
+        }
+
+        public double CalculateCost(double roadLength, double petrolCost)
+        {
+            return CalculateConsump(roadLength) * petrolCost;
+        }
+
+        public void Details()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public static void DisplayCarCount()
+        {
+            Console.WriteLine($"Total cars created: {_carCount}");
+        }
     }
+
 }
---
+++ Lab1/Lab1/Garage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Garage
+    {
+        private Car[]? _cars;
+        string _address;
+        int _carsCount;
+        int _capacity;
+
+        public int Capacity { get { return _capacity; } set { _capacity = value; } }
+
+        public string Address { get { return _address; } set { _address = value; } }
+
+        public Garage()
+        {
+            _address = "nieznany";
+            _carsCount = 0; 
+            _capacity = 0;
+            _cars = null;
+        }
+
+        public Garage(string address, int capacity )
+        {
+            _address = address; 
+            _capacity = capacity;
+            _cars = new Car[_capacity];
+            _carsCount = 0;
+            
+        }
+
+
+        public void CarIn(Car car)
+        {
+            if (_carsCount >= _capacity)
+            {
+                Console.WriteLine("Garaż jest pełny");
+            }
+            else
+            {
+                _cars[_carsCount] = car;
+                _carsCount++;
+                Console.WriteLine($"Samochód {car.Brand} {car.Model} wprowadzony do garażu.");
+            }
+        }
+
+        public Car CarOut()
+        {
+            if ( _carsCount <= 0)
+            {
+                Console.WriteLine("Garaz jest pusty");
+                return null;
+            }
+            else
+            {
+                _carsCount--;
+                Car car = _cars[_carsCount];
+                _cars[_carsCount] = null;
+                Console.WriteLine($"Wyprowadzono samochod {car.Brand} {car.Model}");
+                return car;
+            }
+        }
+
+        public override string ToString()
+        {
+            string carsList = "";
+            foreach (Car car in _cars)
+            {
+                if (car != null)
+                {
+                    carsList += car.ToString() + "\n";
+                }
+            }
+            return $"Garage | Address: {_address}, Capacity: {_capacity}, CarsCount {_carsCount}\nCars:\n{carsList}";
+        }
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+
+
+    }
+}

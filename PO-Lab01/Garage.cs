654e3f62cde2368aff700aac7a443ebadc182100
Marcel Szkop <smth@smth.smth>
2025-04-01 12:36:16 +0200

    PO-Lab01

---
+++ PO-Lab01/Garage.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO_Lab01
+{
+    public class Garage
+    {
+        private Car[] _cars;
+        private string _address;
+        private int _carsCount;
+        private int _capacity;
+
+        public int Capacity
+        {
+            get { return _capacity; }
+            set
+            {
+                _capacity = value;
+                _cars = new Car[value];
+            }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public Garage() // konstruktor domyślny
+        {
+            _cars = null;
+            _address = "nieznany";
+            // _carsCount = 0;
+            _capacity = 0;
+        }
+
+        public Garage(string address, int capacity)
+        {
+            _address = address;
+            _capacity = capacity;
+            _cars = new Car[capacity];
+        }
+
+        public void CarIn(Car car)
+        {
+            if (_carsCount >= _cars.Length)
+            {
+                Console.WriteLine("Garage full");
+            }
+            else
+            {
+                _cars[_carsCount] = car;
+                Console.WriteLine($"Car added on position {_carsCount}");
+                _carsCount++;
+            }
+        }
+
+        public Car CarOut()
+        {
+            if (_carsCount == 0)
+            {
+                Console.WriteLine("Garage empty");
+                return null;
+            }
+            else
+            {
+                _carsCount--;
+                Car removedCar = _cars[_carsCount];
+                _cars[_carsCount] = null;
+                Console.WriteLine($"Car deleted from position {_carsCount}");
+                return removedCar;
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (var car in _cars)
+            {
+                result += car != null ? car.ToString() + "\n" : "brak\n";
+            }
+            return result;
+        }
+
+        public void Details()
+        {
+            Console.WriteLine($"Garage: \n Address: {_address}");
+            if (_carsCount == 0)
+            {
+                Console.WriteLine(" No cars ");
+            }
+            foreach (var car in _cars)
+            {
+                if (car != null) Console.WriteLine(car.ToString());
+            }
+        }
+    }
+}
---
+++ PO-Lab01/Person.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO_Lab01
+{
+    public class Person
+    {
+        //private Car[] cars;
+        private string[] _registrationNumber;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int CarsCount { get => _registrationNumber.Count(r => r != null); }
+        public static int MaxCarCount { get; set; }
+
+        public Person()
+        {
+            FirstName = "nieznany";
+            LastName = "nieznany";
+            Address = "nieznany";
+            //CarsCount = 0;
+            MaxCarCount = 3;
+            _registrationNumber = new string[MaxCarCount];
+        }
+        public Person(string firstName, string lastName, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            //CarsCount = 0;
+            MaxCarCount = 3;
+            _registrationNumber = new string[MaxCarCount];
+        }
+        public Person(string firstName, string lastName, string address, Car[] cars)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            //CarsCount = 0;
+            MaxCarCount = 3;
+            _registrationNumber = new string[MaxCarCount];
+
+            foreach (Car car in cars)
+            {
+                AddCarRegistrationNumber(car.RegistrationNumber);
+            }
+        }
+
+        public void AddCarRegistrationNumber(string registrationNumber)
+        {
+            if (CarsCount < MaxCarCount)
+            {
+                for (int i = 0; i < _registrationNumber.Length; ++i)
+                {
+                    if (_registrationNumber[i] == null)
+                    {
+                        _registrationNumber[i] = registrationNumber;
+                        //CarsCount++;
+                        Console.WriteLine($"Car {registrationNumber} added");
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Registration list full");
+            }
+        }
+
+        public void RemoveCarRegistrationNumber(string registrationNumber)
+        {
+            for (int i = 0; i < _registrationNumber.Length; ++i)
+            {
+                if (_registrationNumber[i] == registrationNumber)
+                {
+                    _registrationNumber[i] = null;
+                    //CarsCount--;
+                    Console.WriteLine($"Car {registrationNumber} removed");
+                    return;
+                }
+            }
+            Console.WriteLine($"Cannot find car {registrationNumber}");
+        }
+
+        public override string ToString()
+        {
+            string registrationList = "";
+            foreach (string registration in _registrationNumber)
+            {
+                if (registration != null)
+                {
+                    registrationList += " " + registration;
+                }
+            }
+            return $"Person: \n FirstName: {FirstName}, LastName: {LastName}, Address: {Address} \n RegistrationList: {registrationList}";
+        }
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+    }
+}

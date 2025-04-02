5032903cdc4be43d34e1940d6c897790c352212e
Bartosz_Zielonka <example@example.org>
2025-04-02 13:40:34 +0200

    zad1 lab4update

--- Lab4/Student.cs
+++ Lab4/Student.cs
@@ -15,7 +15,7 @@
             return Grades.Average(grade => grade.Value);
         }
 
-        public Student(string firstName, string lastName, DateTime dateOfBirth, string specialization, int semester = 1, int group)
+        public Student(string firstName, string lastName, DateTime dateOfBirth, string specialization, int group, int semester = 1)
             : base(firstName, lastName, dateOfBirth)
         {
             Specialization = specialization;

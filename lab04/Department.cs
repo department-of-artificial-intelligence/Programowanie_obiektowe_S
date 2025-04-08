afd6fdcb4b157f6e59109c0f6fa8a76ce4364107
Malgorzata Czx <mc@mc.pl>
2025-04-08 15:42:43 +0200

    zmiany lab04

---
+++ lab04/Department.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab04
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Person Dean { get; set; }
+
+        public IList<OrganizationUnit> Organizations { get; set; }
+
+        public IList<Subject> Subjects { get; set; }
+
+        public IList<Student> Students { get; set; }
+        
+        public Department() { }
+
+        public Department(string name, Person dean, IList<Subject> subjects, IList<Student> students)
+        {
+            Name = name;
+            Dean = dean;
+            Subjects = subjects;
+            Students = students;
+        }
+
+        public override string ToString()
+        {
+            return $" Name: {Name}, Dean: {Dean}, Subjects: {Subjects} and Students: {Students}";
+        }
+
+
+
+
+    }
+}
---
+++ lab04/OrganizationUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab04
+{
+    class OrganizationUnit
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public IList<Lecturer> Lecturers { get; set; }
+        
+        public OrganizationUnit() { }
+
+        public OrganizationUnit(string name, string address, IList<Lecturer> lecturers) {
+        Name = name;
+        Address = address;
+        Lecturers = lecturers;
+        }
+    
+    public override string ToString()
+        {
+            return $"Name: {Name}, Address: {Address} and Lecturers: {Lecturers}";
+        }
+    
+    
+    }
+
+    }
+

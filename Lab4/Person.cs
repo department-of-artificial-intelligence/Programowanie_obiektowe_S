47a584378d5250e4873bffdfc11937eac013c4b4
Jakub_Siwek <loremipsum@gmail.com>
2025-04-08 15:41:43 +0200

    Lab4

---
+++ Lab4/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab4
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
+        public override string ToString()
+        {
+            return $"Person | FirstName: {FirstName}, LastName: {LastName}, DateOfBirth: {DateOfBirth}";
+        }
+    }
+}

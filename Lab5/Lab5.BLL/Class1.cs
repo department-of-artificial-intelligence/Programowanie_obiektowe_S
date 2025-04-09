84083d86d8c1a54aa499a59a2d93cebd49517e43
Bartosz_Zielonka <example@example.org>
2025-04-09 13:39:38 +0200

    initial

---
+++ Lab5/Lab5.BLL/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab5.BLL
+{
+    public abstract class Class1
+    {
+    }
+}
---
+++ Lab5/Lab5.BLL/Zoo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab5.BLL
+{
+    public class Bird : Animal
+    {
+    }
+
+    public class Zoo
+    {
+    }
+}

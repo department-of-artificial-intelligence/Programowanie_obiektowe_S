8bd373f33b0f9dc01555bdc9c8624f609a22e092
Maciej_Pecina <smth@smth.smth>
2025-04-15 13:44:47 +0200

    lab7

---
+++ lab07/lab07.WpfApp/Calculator.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab07.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonNuber(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button Buttonx) 
+            {
+                TextBlock.Text += Buttonx.Content;
+            }
+        }
+
+        private void ButtonClear(object sender, RoutedEventArgs e)
+        {
+            TextBlock.Text = " ";
+        }
+    }
+}

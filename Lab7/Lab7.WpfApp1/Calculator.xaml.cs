49e62d27c71afc07e207d3c6586c6e8f7e760e37
Piotr_Krzysinski <smth@smth.smth>
2025-04-16 13:42:38 +0200

    lab7 start

---
+++ Lab7/Lab7.WpfApp1/Calculator.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab7.WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

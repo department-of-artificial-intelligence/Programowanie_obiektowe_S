5b1550aa18318a9960237056d0ac839243a4c4d1
Bartosz_Zielonka <example@example.org>
2025-04-16 13:42:42 +0200

    initial commit

---
+++ Lab7/Lab7.WpfApp/Calculator.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab7.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        private bool IsResult = false;
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            LabelResult.Content = LabelResult.Content?.ToString() + btn?.Content.ToString();
+        }
+
+        private void ButtonClickClear(object sender, RoutedEventArgs e)
+        {
+            LabelResult.Content = "";
+        }
+
+        private void ButtonClickEquals(object sender, RoutedEventArgs e)
+        {
+            string expression = LabelResult.Content?.ToString();
+
+            try
+            {
+                var result = new DataTable().Compute(expression, null);
+                LabelResult.Content = result.ToString();
+            }
+            catch
+            {
+                LabelResult.Content = "Błąd";
+            }
+        }
+    }
+}

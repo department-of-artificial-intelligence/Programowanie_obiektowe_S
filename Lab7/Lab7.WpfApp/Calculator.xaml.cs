792474be87dde7281a62652642f9e99f90525599
Aleksandra_Papis <ap@ap.com>
2025-04-16 13:40:18 +0200

    kalkulator

---
+++ Lab7/Lab7.WpfApp/Calculator.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab7.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void _0_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        private void _1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _floating_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _plus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _clear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _multiply_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _minus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _divide_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _equals_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab7/Lab7.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab7.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button buttonOk)
+                buttonOk.Content += TextBoxName.Text;
+        }
+    }
+}

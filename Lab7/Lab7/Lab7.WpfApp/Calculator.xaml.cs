6f4df8d82e6356f249a1c64b17a9c8f432d3e580
Adam Bednarski <smth@smth.smth>
2025-05-05 22:15:37 +0200

    Lab 7 Koniec

---
+++ Lab7/Lab7/Lab7.WpfApp/Calculator.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Lab7.BLL;
+
+namespace Lab7.WpfApp
+{
+    public partial class Calculator : Window
+    {
+        private CalculatorLogic _logic = new CalculatorLogic();
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                _logic.EnterNumber(btn.Content.ToString());
+                Display.Text = _logic.Display;
+            }
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                _logic.SetOperator(btn.Content.ToString());
+                Display.Text = _logic.Display;
+            }
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            _logic.Calculate();
+            Display.Text = _logic.Display;
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            _logic.Clear();
+            Display.Text = _logic.Display;
+        }
+
+        private void Percentage_Click(object sender, RoutedEventArgs e)
+        {
+            _logic.Percentage();
+            Display.Text = _logic.Display;
+        }
+    }
+}

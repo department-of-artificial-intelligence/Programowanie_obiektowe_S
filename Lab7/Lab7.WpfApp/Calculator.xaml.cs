0d5ac5079edc1a79cd92d1ec8a0856653da48075
Wiktoria138252 <smith@smith.smith>
2025-05-09 21:39:23 +0200

    Lab7

---
+++ Lab7/Lab7.WpfApp/Calculator.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalculatorApp
+{
+    public partial class MainWindow : Window
+    {
+        private string input = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                input += button.Content.ToString();
+                Display.Text = input;
+            }
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                input += $" {button.Content} ";
+                Display.Text = input;
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            input = "";
+            Display.Text = "0";
+        }
+
+        private void Equal_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var result = new DataTable().Compute(input, null);
+                string resultString = result.ToString().Replace(',', '.');
+                Display.Text = resultString;
+                input = resultString;
+            }
+            catch
+            {
+                Display.Text = "Error";
+                input = "";
+            }
+        }
+    }
+}

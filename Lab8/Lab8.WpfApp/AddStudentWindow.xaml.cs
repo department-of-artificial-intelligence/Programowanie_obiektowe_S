11575d5a1025e7c6bfb609ba761a72ecff646b06
Jakub_Siwek <loremipsum@gmail.com>
2025-05-13 15:41:07 +0200

    Gites

---
+++ Lab8/Lab8.WpfApp/AddStudentWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Lab8.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab8.WpfApp
+{
+    public partial class AddStudentWindow : Window
+    {
+        public Student Student { get; set; }
+        public AddStudentWindow(Student student = null)
+        {
+            InitializeComponent();
+            if(student != null)
+            {
+                FacultyTextBox.Text = student.Faculty;
+                FirstNameTextBox.Text = student.FirstName;
+                SurNameTextBox.Text = student.SurName;
+                StudentsNoTextBox.Text = student.StudentNo.ToString();
+            }
+            Student = student ?? new Student();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Student.FirstName = FirstNameTextBox.Text;
+            Student.SurName = SurNameTextBox.Text;
+            int tmp = 0;
+            int.TryParse(StudentsNoTextBox.Text, out tmp);
+            Student.StudentNo = tmp;
+            Student.Faculty = FacultyTextBox.Text;
+            DialogResult = true;
+        }
+    }
+}
---
+++ Lab8/Lab8.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Lab8.BLL;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab8.WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        IList<Student> Students {  get; set; }
+        AddStudentWindow Window { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Students = new List<Student>
+            {
+                new Student(){FirstName="Jan", SurName="Kowalski", Faculty="WIMII", StudentNo=1000},
+                new Student(){FirstName="Anna", SurName="Kowalska", Faculty="WIMII", StudentNo=1001},
+                new Student(){FirstName="Jan", SurName="Nowak", Faculty="WIMII", StudentNo=1002},
+                new Student(){FirstName="Jana", SurName="Nowak", Faculty="WIMII", StudentNo=1003}
+            };
+
+            DataGridStudents.ItemsSource = Students;
+            DataGridStudents.AutoGenerateColumns = true;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window = new AddStudentWindow();
+            if (Window.ShowDialog() ?? false) Students.Add(Window.Student);
+            DataGridStudents.Items.Refresh();
+        }
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(DataGridStudents.SelectedItem is Student student)
+            {
+                Students.Remove(student);
+                DataGridStudents.Items.Refresh();
+            }
+        }
+    }
+}

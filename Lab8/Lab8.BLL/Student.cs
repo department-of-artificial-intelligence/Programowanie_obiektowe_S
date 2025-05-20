cb5026c7836d1f1baafc85edd4beaa096c25e28b
Daksym_Domilovskyi <smth@smth.smth>
2025-05-20 13:46:32 +0200

    lab8 poczatek

---
+++ Lab8/Lab8.BLL/Student.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Student {
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Index { get; set; }
+    public string Department { get; set; }
+}
--- Lab8/Lab8.WpfApp/MainWindow.xaml.cs
+++ Lab8/Lab8.WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,10 +13,28 @@ using System.Windows.Shapes;
 namespace Lab8.WpfApp {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary> 
+
     public partial class MainWindow : Window {
+        public IList<Student> Students { get; set; }
+
         public MainWindow() {
             InitializeComponent();
+            Students = new List<Student> {
+                new Student(){ Name = "Jan", Surname = "Kowalski", Index = 1010, Department = "WIMII" },
+                new Student(){ Name = "Michał", Surname = "Nowak", Index = 1011, Department = "WIMII" },
+                new Student(){ Name = "Jacek", Surname = "Makieta", Index = 1012, Department = "WIMII" }
+            };
+            Tabela.Columns.Add(new DataGridTextColumn() { Header = "Name", Binding = new Binding("Name") });
+            Tabela.Columns.Add(new DataGridTextColumn() { Header = "Surname", Binding = new Binding("Surname") });
+            Tabela.Columns.Add(new DataGridTextColumn() { Header = "Index", Binding = new Binding("Index") });
+            Tabela.Columns.Add(new DataGridTextColumn() { Header = "Department", Binding = new Binding("Department") });
+            Tabela.AutoGenerateColumns = false;
+            Tabela.ItemsSource = Students;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
         }
     }
 }

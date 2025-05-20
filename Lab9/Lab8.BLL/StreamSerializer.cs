ee1bd76bc9924526997bdd70236f31b2e5689fc9
Maciej_Pecina <smth@smth.smth>
2025-05-20 13:46:00 +0200

    lab09_2

---
+++ Lab9/Lab8.BLL/StreamSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8.BLL
+{
+    public static class StreamSerializer
+    {
+        public static async Task Save<T>(this StreamWriter streamWriter, T obj)
+        {
+            Type objType = obj.GetType();
+            await streamWriter.WriteLineAsync($"[[{objType.AssemblyQualifiedName}]]");
+            foreach (var propertyInfo in objType.GetProperties())
+            {
+                //if (propertyInfo.GetCustomAttribute<TableAttribute>() == null)
+                //{
+                //    await streamWriter.WriteLineAsync($"[{propertyInfo.Name}]");
+                //    await streamWriter.WriteLineAsync(propertyInfo.GetValue(obj)?.ToString());
+                //}
+            }
+            await streamWriter.WriteLineAsync("[[TEST]]");
+        }
+    }
+}
---
+++ Lab9/Lab8.WpfApp/AddStudentWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Lab8.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab8.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for AddStudentWindow.xaml
+    /// </summary>
+    public partial class AddStudentWindow : Window
+    {
+        public Student Student { get; set; }
+        public AddStudentWindow(Student student = null)
+        {
+            InitializeComponent();
+            if (student != null )
+            {
+                TextBoxFaculty.Text = student.Faculty;
+                TextBoxFirstname.Text = student.Firstname;
+                TextBoxStudentNo.Text = student.StudentNo.ToString();
+                TextBoxSurname.Text = student.Surname;
+            }
+            Student = student ?? new Student();
+        }
+
+        private void ButtonAddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Regex.IsMatch(TextBoxFirstname.Text, @"^\p{L}{1,12}$") ||
+                !Regex.IsMatch(TextBoxSurname.Text, @"^\p{L}{1,12}$") ||
+                !Regex.IsMatch(TextBoxFaculty.Text, @"^\p{L}{1,12}$") ||
+                !Regex.IsMatch(TextBoxStudentNo.Text, @"^[0-9]{4,10}$"))
+            {
+                MessageBox.Show("Invalid input data.");
+                return;
+            }
+
+            Student.Firstname = TextBoxFirstname.Text;
+            Student.Surname = TextBoxSurname.Text;
+            Student.Faculty = TextBoxFaculty.Text;
+
+            if (!int.TryParse(TextBoxStudentNo.Text, out int studentNo))
+                MessageBox.Show("Student is not a number.");
+            
+            Student.StudentNo = studentNo;
+            DialogResult = true;
+        }
+    }
+}

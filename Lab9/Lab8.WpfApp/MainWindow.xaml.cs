aa16f7d6bfdca0ac0e944a130e4cbd9c810442c3
michalzu <mjarzabkiewicz.mikolaj@gmail.com>
2025-06-03 13:44:19 +0200

    Destroyed Mentally

--- Lab9/Lab8.WpfApp/MainWindow.xaml.cs
+++ Lab9/Lab8.WpfApp/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lab9.WpfApp
 {
@@ -55,6 +48,71 @@ namespace Lab9.WpfApp
                 DataGridStudents.Items.Refresh();
                 return;
             }
+        }
+
+        private void BtnCloseStream_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLoadStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLoadTxtStream_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /*
+        private void BtnSaveTxt_Click(object sender, RoutedEventArgs e)
+        {
+            FileStream fs = new FileStream("D:\\LocalAccounts\\Student\\Documents\\Szymon_Kupczyk\\Lab9\\Lab8.WpfApp\\data.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+
+            var properties = Students[0].GetType().GetProperties();
+            foreach (var student in Students)
+            {
+                sw.WriteLine("[[Student]]");
+                foreach (var property in properties)
+                {
+                    if (property.Name != "Grades")
+                    {
+                        sw.WriteLine("[" + property.Name + "]");
+                        sw.WriteLine(property.GetValue(Students));
+                    }
+                }
+                sw.WriteLine("[[]]");
+            }
+            sw.Close();
+            MessageBox.Show("Saved");
+        }
+        */
+
+        private void BtnLoadTxt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSaveXML_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLoadXML_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSaveJSON_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLoadJSON_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
